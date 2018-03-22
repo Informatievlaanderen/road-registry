a9ef312bd8a9ec910d1305a95ae21e6199556fa8
Yves Reynhout <yves.reynhout@kb.vlaanderen.be>
2018-03-22 12:39:40 +0100

    Clarified mapping of road node type.

--- src/RoadRegistry/Commands/RoadNodeType.cs
+++ src/RoadRegistry/Commands/RoadNodeType.cs
@@ -2,10 +2,11 @@ namespace RoadRegistry.Commands
 {
     public enum RoadNodeType
     {
-        RealNode, //echte knoop
-        FakeNode, //schijnknoop
-        EndNode, //eindknoop
-        MiniRoundabout, //mini rotonde
-        TurnLoopNode // keerlusknoop
+
+        RealNode, // 1	echte knoop	Punt waar 2 wegsegmenten elkaar snijden; minstens drie aansluitende wegsegmenten.
+        FakeNode, // 2	schijnknoop	Punt waar 2 wegsegmenten elkaar raken; slechts twee aansluitende wegsegmenten.
+        EndNode, // 3	eindknoop	Het einde van een doodlopende wegcorridor, slechts één aansluitend wegsegment.
+        MiniRoundabout, // 4	minirotonde	Kruispunt dat zich in de realiteit voordoet als een rotonde maar niet voldoet aan de geometrische specificaties om opgenomen te worden als een echte rotonde (ringvormige geometrie).
+        TurnLoopNode // 5	keerlusknoop	Juist twee aansluitende wegsegmenten; wegsegmenten die aan beide zijden begrensd worden door dezelfde wegknoop worden met behulp van een extra wegknoop (= keerlusknoop) opgesplitst.
     }
 }
