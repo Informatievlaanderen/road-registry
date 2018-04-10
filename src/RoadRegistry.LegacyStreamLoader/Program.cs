b4ceca6d4f24e192566e901b1dc1ddec38693e5e
Yves Reynhout <yves.reynhout@kb.vlaanderen.be>
2018-04-10 15:33:54 +0200

    Introducing the legacy stream loader (which will write a packaged road network stream as json to the event store).

---
+++ src/RoadRegistry.LegacyStreamLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoadRegistry.LegacyStreamLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
