314c760e2ae0ecc18aaef9b9dcadb8e1a2b58cd8
Yves Reynhout <yves.reynhout@kb.vlaanderen.be>
2018-04-27 16:06:18 +0200

    Duplicated OSLO projection projects to transform into SHAPE projections.

---
+++ src/RoadRegistry.Projections/OsloRunnerModule.cs
@@ -0,0 +1,52 @@
+namespace RoadRegistry.Projections.Oslo
+{
+    using Aiv.Vbr.AggregateSource.SqlStreamStore.Autofac;
+    using Aiv.Vbr.EventHandling;
+    using Aiv.Vbr.EventHandling.Autofac;
+    using Aiv.Vbr.ProjectionHandling.SqlStreamStore.Autofac;
+    using Autofac;
+    using Autofac.Extensions.DependencyInjection;
+    using Infrastructure;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    public class OsloRunnerModule : Module
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IServiceCollection _services;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public OsloRunnerModule(
+            IConfiguration configuration,
+            IServiceCollection services,
+            ILoggerFactory loggerFactory)
+        {
+            _configuration = configuration;
+            _services = services;
+            _loggerFactory = loggerFactory;
+        }
+
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            var eventSerializerSettings = EventsJsonSerializerSettingsProvider.CreateSerializerSettings();
+
+            containerBuilder
+                .RegisterModule(new OsloModule(_configuration, _services, _loggerFactory));
+
+            containerBuilder
+                .RegisterModule(new EventHandlingModule(typeof(DomainAssemblyMarker).Assembly, eventSerializerSettings));
+
+            containerBuilder
+                .RegisterModule(new EnvelopeModule());
+
+            containerBuilder
+                .RegisterModule(new SqlStreamStoreModule(_configuration.GetConnectionString("Events"), Schema.Default));
+
+            containerBuilder.RegisterType<RoadOsloRunner>()
+                .SingleInstance();
+
+            containerBuilder.Populate(_services);
+        }
+    }
+}
---
+++ src/RoadRegistry.Projections/RoadList/RoadList.cs
@@ -0,0 +1,45 @@
+namespace RoadRegistry.Projections.Oslo.RoadList
+{
+    using System;
+    using Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using ValueObjects;
+
+    public class RoadListItem
+    {
+        public Guid? RoadId { get; set; }
+
+        public Language? PrimaryLanguage { get; set; }
+        public Language? SecondaryLanguage { get; set; }
+
+        public string DefaultName { get; set; }
+        public string NameDutch { get; set; }
+        public string NameFrench { get; set; }
+        public string NameGerman { get; set; }
+        public string NameEnglish { get; set; }
+    }
+
+    public class RoadListConfiguration : IEntityTypeConfiguration<RoadListItem>
+    {
+        private const string TableName = "RoadList";
+
+        public void Configure(EntityTypeBuilder<RoadListItem> b)
+        {
+            b.ToTable(TableName, Schema.Oslo)
+                .HasKey(p => p.RoadId)
+                .ForSqlServerIsClustered(false);
+
+            b.Property(p => p.PrimaryLanguage);
+            b.Property(p => p.SecondaryLanguage);
+
+            b.Property(p => p.DefaultName);
+            b.Property(p => p.NameDutch);
+            b.Property(p => p.NameFrench);
+            b.Property(p => p.NameGerman);
+            b.Property(p => p.NameEnglish);
+
+            b.HasIndex(p => p.DefaultName).ForSqlServerIsClustered();
+        }
+    }
+}
