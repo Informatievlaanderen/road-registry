f8b32247517037ec6b299fa4711eb07988142c9e
Yves Reynhout <yves.reynhout@kb.vlaanderen.be>
2018-06-01 11:38:52 +0200

    Switched to new way of defining records for road node dbase record.

--- src/RoadRegistry.Projections/RoadNodeShape/RoadNodeDbaseRecord.cs
+++ src/RoadRegistry.Projections/RoadNodeShape/RoadNodeDbaseRecord.cs
@@ -4,7 +4,7 @@ using Shaperon;
 
 namespace RoadRegistry.Projections
 {
-    public class RoadNodeDbaseRecord
+    public class RoadNodeDbaseRecord : DbaseRecord
     {
         public static readonly RoadNodeDbaseSchema Schema = new RoadNodeDbaseSchema();
 
@@ -17,7 +17,11 @@ namespace RoadRegistry.Projections
             BEGINTIJD = new DbaseDateTime(Schema.WK_UIDN);
             BEGINORG = new DbaseString(Schema.WK_UIDN);
             LBLBGNORG = new DbaseString(Schema.WK_UIDN);
-            IsDeleted = false;
+
+            Values = new DbaseFieldValue[]
+            {
+                WK_OIDN,WK_UIDN,TYPE,LBLTYPE,BEGINTIJD,BEGINORG,LBLBGNORG
+            };
         }
 
         public DbaseInt32 WK_OIDN { get; }
@@ -27,61 +31,5 @@ namespace RoadRegistry.Projections
         public DbaseDateTime BEGINTIJD { get; }
         public DbaseString BEGINORG { get; }
         public DbaseString LBLBGNORG { get; }
-        public bool IsDeleted { get; set; }
-
-        public void Read(BinaryReader reader)
-        {
-            if (reader == null)
-            {
-                throw new ArgumentNullException(nameof(reader));
-            }
-
-            var flag = reader.ReadByte();
-            if(flag == DbaseRecord.EndOfFile)
-            {
-                throw new DbaseFileHeaderException("The end of file was reached unexpectedly.");
-            }
-            if(flag != 0x20 && flag != 0x2A)
-            {
-                throw new DbaseFileHeaderException($"The record deleted flag must be either deleted (0x2A) or valid (0x20) but is 0x{flag:X2}");
-            }
-            IsDeleted = flag == 0x2A;
-            WK_OIDN.Read(reader);
-            WK_UIDN.Read(reader);
-            TYPE.Read(reader);
-            LBLTYPE.Read(reader);
-            BEGINTIJD.Read(reader);
-            BEGINORG.Read(reader);
-            LBLBGNORG.Read(reader);
-        }
-
-        public void Write(BinaryWriter writer)
-        {
-            if (writer == null)
-            {
-                throw new ArgumentNullException(nameof(writer));
-            }
-            writer.Write(Convert.ToByte(IsDeleted ? 0x2A : 0x20));
-            WK_OIDN.Write(writer);
-            WK_UIDN.Write(writer);
-            TYPE.Write(writer);
-            LBLTYPE.Write(writer);
-            BEGINTIJD.Write(writer);
-            BEGINORG.Write(writer);
-            LBLBGNORG.Write(writer);
-        }
-
-        public byte[] ToBytes()
-        {
-            using(var output = new MemoryStream())
-            {
-                using(var writer = new BinaryWriter(output))
-                {
-                    Write(writer);
-                    writer.Flush();
-                }
-                return output.ToArray();
-            }
-        }
     }
 }
