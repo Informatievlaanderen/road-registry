4f4d95d6ac829adb404bc269cd1f9e82af059dd6
Wouter De Rijck <wouter.derijck@kb.vlaanderen.be>
2018-08-16 14:29:31 +0200

    introcude type translation inconsistencies for to generate exact file copies

--- src/RoadRegistry/Events/RoadSegmentStatus.cs
+++ src/RoadRegistry/Events/RoadSegmentStatus.cs
@@ -33,7 +33,7 @@ namespace RoadRegistry.Events
             {
                 { RoadSegmentStatus.Unknown, "Geen informatie beschikbaar" },
                 { RoadSegmentStatus.PermitRequested, "Geometrie komt voor op officieel document in behandeling." },
-                { RoadSegmentStatus.BuildingPermitGranted, "Geometrie komt voor op goedgekeurd, niet vervallen bouwdossier." },
+                { RoadSegmentStatus.BuildingPermitGranted, "Geometrie komt voor op goedgekeurd, niet vervallen bouwdossier" },
                 { RoadSegmentStatus.UnderConstruction, "Aanvang der werken is gemeld." },
                 { RoadSegmentStatus.InUse, "Werken zijn opgeleverd." },
                 { RoadSegmentStatus.OutOfUse, "Fysieke weg is buiten gebruik gesteld maar niet gesloopt." },
