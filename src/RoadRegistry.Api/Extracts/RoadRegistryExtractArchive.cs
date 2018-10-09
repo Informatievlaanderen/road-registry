5f855f6422d5bf7b8fea6dfa170e2f9da86cab31
Wouter De Rijck <wouter.derijck@kb.vlaanderen.be>
2018-10-09 13:58:48 +0200

    clean up unsused ref

--- src/RoadRegistry.Api/Extracts/RoadRegistryExtractArchive.cs
+++ src/RoadRegistry.Api/Extracts/RoadRegistryExtractArchive.cs
@@ -3,7 +3,6 @@ namespace RoadRegistry.Api.Extracts
     using System;
     using System.Collections;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
     using System.IO.Compression;
     using System.Linq;
