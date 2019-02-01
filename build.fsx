eb26b71bb329fb0e0268e786db95847bd273e7b0
Yves Reynhout <info@bittacklr.be>
2019-02-01 12:42:05 +0100

    Cleaned up build script.

--- build.fsx
+++ build.fsx
@@ -5,124 +5,113 @@ open Fake.NpmHelper
 open ``Build-generic``
 
 let dockerRepository = "roadregistry"
-let assemblyVersionNumber = (sprintf "2.0.0.%s")
-let nugetVersionNumber = (sprintf "2.0.%s")
+let assemblyVersionNumber = (sprintf "2.%s")
+let nugetVersionNumber = (sprintf "%s")
 
-let containerize = containerize dockerRepository
-let push = push dockerRepository
-let build = build assemblyVersionNumber
+let build = buildSolution assemblyVersionNumber
+let test = testSolution
 let publish = publish assemblyVersionNumber
 let pack = pack nugetVersionNumber
+let containerize = containerize dockerRepository
+let push = push dockerRepository
 
-Target "Build_CoreComponents" (fun _ ->
-  [
-    "RoadRegistry.BackOffice"
-  ] |> List.iter build
-)
-Target "Test_CoreComponents" (fun _ ->
-  [
-    "test" @@ "RoadRegistry.BackOffice.Tests"
-  ] |> List.iter testWithXunit
-)
+// Solution -----------------------------------------------------------------------
 
-// Legacy stream extraction
-Target "Build_LegacyDataExtraction" (fun _ -> build "RoadRegistry.LegacyStreamExtraction")
-Target "Test_LegacyDataExtraction" DoNothing
-Target "Publish_LegacyDataExtraction" (fun _ -> publish "RoadRegistry.LegacyStreamExtraction")
+Target "Restore_Solution" (fun _ -> restore "RoadRegistry")
 
-// Legacy stream loader
-Target "Build_LegacyDataLoader" (fun _ -> build "RoadRegistry.LegacyStreamLoader")
-Target "Test_LegacyDataLoader" DoNothing
-Target "Publish_LegacyDataLoader" (fun _ -> publish "RoadRegistry.LegacyStreamLoader")
-Target "Package_LegacyDataLoader" (fun _ -> containerize "RoadRegistry.LegacyStreamLoader" "legacy-stream-loader")
-Target "PushContainer_LegacyDataLoader" (fun _ -> push "legacy-stream-loader")
-
-// Projections
-Target "Build_Projections" (fun _ -> build "RoadRegistry.BackOffice.Projections")
-Target "Test_Projections" DoNothing
-Target "Publish_Projections" (fun _ -> publish "RoadRegistry.BackOffice.Projections")
-Target "Package_Projections" (fun _ -> containerize "RoadRegistry.BackOffice.Projections" "projections-legacy")
-Target "PushContainer_Projections" (fun _ -> push "projections-legacy")
-
-// Site (api + ui)
-Target "Clean_Site" (fun _ -> 
+Target "Build_Solution" (fun _ ->
   CleanDir ("src" @@ "RoadRegistry.UI" @@ "wwwroot")
-)
-Target "Build_Site" (fun _ ->
-  let uiProjectDirectory = "src" @@ "RoadRegistry.UI"
+
   Npm (fun p ->
     { p with
         Command = Install Standard
-        WorkingDirectory = uiProjectDirectory
+        WorkingDirectory = "src" @@ "RoadRegistry.UI"
     }
   )
+
   Npm (fun p ->
     { p with
         Command = (Run "build")
-        WorkingDirectory = uiProjectDirectory
+        WorkingDirectory = "src" @@ "RoadRegistry.UI"
     }
   )
 
+  build "RoadRegistry"
+)
+
+Target "Test_Solution" (fun _ -> test "RoadRegistry")
+
+Target "Publish_Solution" (fun _ ->
   [
+    "RoadRegistry.BackOffice"
+    "RoadRegistry.BackOffice.Schema"
+    "RoadRegistry.BackOffice.Projections"
+    "RoadRegistry.LegacyStreamLoader"
+    "RoadRegistry.LegacyStreamExtraction"
     "RoadRegistry.BackOffice.Api"
     "RoadRegistry.UI"
-  ] |> List.iter build
-)
-Target "Test_Site" DoNothing
-Target "Publish_Site" (fun _ ->
+  ] |> List.iter publish)
+
+Target "Pack_Solution" (fun _ ->
   [
+    "RoadRegistry.BackOffice"
+    "RoadRegistry.BackOffice.Schema"
+    "RoadRegistry.BackOffice.Projections"
+    "RoadRegistry.LegacyStreamLoader"
+    "RoadRegistry.LegacyStreamExtraction"
     "RoadRegistry.BackOffice.Api"
     "RoadRegistry.UI"
-  ] |> List.iter publish
-)
-Target "Package_Site" (fun _ ->
-  [
-    ("RoadRegistry.BackOffice.Api", "api-legacy")
-    ("RoadRegistry.UI", "ui")
-  ] |> List.iter (fun (project, containerName) -> containerize project containerName)
-)
-Target "PushContainer_Site" (fun _ ->
-  [
-    "api-legacy"
-    "ui"
-  ] |> List.iter push
-)
+  ] |> List.iter pack)
 
-// Combined Targets
-Target "BuildCore" DoNothing
-Target "PublishAll" DoNothing
-Target "PackageAll" DoNothing
-Target "PushAll" DoNothing
+Target "Containerize_Api" (fun _ -> containerize "RoadRegistry.BackOffice.Api" "backoffice-api")
+Target "PushContainer_Api" (fun _ -> push "backoffice-api")
 
-// -- TARGET DEPENDENCIES --
-// Publish artefacts to build folder
-"DotNetCli" ==> "Clean" ==> "Clean_Site" ==> "Restore" ==> "Build_CoreComponents" ==> "Test_CoreComponents" ==> "BuildCore"
-"BuildCore" ==> "Build_LegacyDataExtraction" ==> "Test_LegacyDataExtraction" ==> "Publish_LegacyDataExtraction"
-"BuildCore" ==> "Build_LegacyDataLoader" ==> "Test_LegacyDataLoader" ==> "Publish_LegacyDataLoader"
-"BuildCore" ==> "Build_Projections" ==> "Test_Projections" ==> "Publish_Projections"
-"BuildCore" ==> "Build_Site" ==> "Test_Site" ==> "Publish_Site"
+Target "Containerize_UI" (fun _ -> containerize "RoadRegistry.UI" "backoffice-ui")
+Target "PushContainer_UI" (fun _ -> push "backoffice-ui")
 
-"Publish_LegacyDataExtraction" ==> "PublishAll"
-"Publish_LegacyDataLoader" ==> "PublishAll"
-"Publish_Projections" ==> "PublishAll"
-"Publish_Site" ==> "PublishAll"
+Target "Containerize_Projections" (fun _ -> containerize "RoadRegistry.BackOffice.Projections" "backoffice-projections")
+Target "PushContainer_Projections" (fun _ -> push "backoffice-projections")
 
-// Package the builds into local Docker images
-"Publish_LegacyDataLoader" ==> "Package_LegacyDataLoader"
-"Publish_Projections" ==> "Package_Projections"
-"Publish_Site" ==> "Package_Site"
+Target "Containerize_LegacyStreamLoader" (fun _ -> containerize "RoadRegistry.LegacyStreamLoader" "legacystreamloader")
+Target "PushContainer_LegacyStreamLoader" (fun _ -> push "legacystreamloader")
 
-"Package_LegacyDataLoader" ==> "PackageAll"
-"Package_Projections" ==> "PackageAll"
-"Package_Site" ==> "PackageAll"
+// --------------------------------------------------------------------------------
 
-// Push ends up with Docker images in AWS
-"Package_LegacyDataLoader" ==> "DockerLogin" ==> "PushContainer_LegacyDataLoader"
-"Package_Projections" ==> "DockerLogin" ==> "PushContainer_Projections"
-"Package_Site" ==> "DockerLogin" ==> "PushContainer_Site"
+Target "Build" DoNothing
+Target "Test" DoNothing
+Target "Publish" DoNothing
+Target "Pack" DoNothing
+Target "Containerize" DoNothing
+Target "Push" DoNothing
 
-"PushContainer_LegacyDataLoader" ==> "PushAll"
-"PushContainer_Projections" ==> "PushAll"
-"PushContainer_Site" ==> "PushAll"
+"DotNetCli"          ==> "Build"
+"Clean"              ==> "Build"
+"Restore_Solution"   ==> "Build"
+"Build_Solution"     ==> "Build"
 
-RunTargetOrDefault "PackageAll"
+"Build"              ==> "Test"
+"Test_Solution"      ==> "Test"
+
+"Test"               ==> "Publish"
+"Publish_Solution"   ==> "Publish"
+
+"Publish"            ==> "Pack"
+"Pack_Solution"      ==> "Pack"
+
+"Pack"                                    ==> "Containerize"
+"Containerize_Api"                        ==> "Containerize"
+"Containerize_UI"                         ==> "Containerize"
+"Containerize_Projections"                ==> "Containerize"
+"Containerize_LegacyStreamLoader"         ==> "Containerize"
+// Possibly add more projects to containerize here
+
+"Containerize"                            ==> "Push"
+"DockerLogin"                             ==> "Push"
+"PushContainer_Api"                       ==> "Push"
+"PushContainer_UI"                        ==> "Push"
+"PushContainer_Projections"               ==> "Push"
+"PushContainer_LegacyStreamLoader"        ==> "Push"
+// Possibly add more projects to push here
+
+// By default we build & test
+RunTargetOrDefault "Test"
