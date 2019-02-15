921a2dfb154d0a3ea6b3a8a27da1918075746e74
Yves Reynhout <info@bittacklr.be>
2019-02-15 10:17:55 +0100

    Fixed ErrorComparer. More tests for the ZipArchiveDbaseEntryValidator.

--- test/RoadRegistry.BackOffice.Tests/Translation/ZipArchiveDbaseEntryValidatorTests.cs
+++ test/RoadRegistry.BackOffice.Tests/Translation/ZipArchiveDbaseEntryValidatorTests.cs
@@ -6,6 +6,7 @@ namespace RoadRegistry.BackOffice.Translation
     using System.IO.Compression;
     using System.Linq;
     using System.Text;
+    using AutoFixture;
     using Be.Vlaanderen.Basisregisters.Shaperon;
     using Model;
     using Xunit;
@@ -54,7 +55,7 @@ namespace RoadRegistry.BackOffice.Translation
         }
 
         [Fact]
-        public void ValidateReturnsExpectedResultWhenEntryIsEmptyStream()
+        public void ValidateReturnsExpectedResultWhenEntryStreamIsEmpty()
         {
             var sut = new ZipArchiveDbaseEntryValidator<FakeDbaseRecord>(
                 Encoding.Default,
@@ -77,14 +78,126 @@ namespace RoadRegistry.BackOffice.Translation
                     var result = sut.Validate(entry);
 
                     Assert.Equal(
-                        ZipArchiveErrors.None.DbaseHeaderFormatError(entry.Name, new EndOfStreamException()),
+                        ZipArchiveErrors.None.DbaseHeaderFormatError(
+                            entry.Name,
+                            new EndOfStreamException("Unable to read beyond the end of the stream.")),
+                        result,
+                        new ErrorComparer());
+                }
+            }
+        }
+
+        [Fact]
+        public void ValidateReturnsExpectedResultWhenEntryStreamContainsMalformedDbaseHeader()
+        {
+            var sut = new ZipArchiveDbaseEntryValidator<FakeDbaseRecord>(
+                Encoding.Default,
+                new FakeDbaseSchema(),
+                new FakeDbaseRecordValidator());
+
+            using (var stream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(stream, ZipArchiveMode.Create, true))
+                {
+                    var entry = archive.CreateEntry("entry");
+                    using (var entryStream = entry.Open())
+                    {
+                        entryStream.WriteByte(0);
+                        entryStream.Flush();
+                    }
+                }
+                stream.Flush();
+                stream.Position = 0;
+
+                using (var archive = new ZipArchive(stream, ZipArchiveMode.Read, true))
+                {
+                    var entry = archive.GetEntry("entry");
+
+                    var result = sut.Validate(entry);
+
+                    Assert.Equal(
+                        ZipArchiveErrors.None.DbaseHeaderFormatError(
+                            entry.Name,
+                            new DbaseFileHeaderException("The database file type must be 3 (dBase III).")),
+                        result,
+                        new ErrorComparer());
+                }
+            }
+        }
+
+        [Fact]
+        public void ValidateReturnsExpectedResultWhenDbaseFileHeaderDoesNotMatch()
+        {
+            var sut = new ZipArchiveDbaseEntryValidator<FakeDbaseRecord>(
+                Encoding.UTF8,
+                new FakeDbaseSchema(),
+                new FakeDbaseRecordValidator());
+            var date = DateTime.Today;
+            var header = new DbaseFileHeader(
+                date,
+                DbaseCodePage.Western_European_ANSI,
+                new DbaseRecordCount(0),
+                new AnonymousDbaseSchema(new DbaseField[0]));
+
+            using (var stream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(stream, ZipArchiveMode.Create, true))
+                {
+                    var entry = archive.CreateEntry("entry");
+                    using(var entryStream = entry.Open())
+                    using(var writer = new BinaryWriter(entryStream, Encoding.UTF8))
+                    {
+                        header.Write(writer);
+                        entryStream.Flush();
+                    }
+                }
+                stream.Flush();
+                stream.Position = 0;
+
+                using (var archive = new ZipArchive(stream, ZipArchiveMode.Read, true))
+                {
+                    var entry = archive.GetEntry("entry");
+
+                    var result = sut.Validate(entry);
+
+                    Assert.Equal(
+                        ZipArchiveErrors.None.DbaseSchemaMismatch(
+                            entry.Name,
+                            new FakeDbaseSchema(),
+                            new AnonymousDbaseSchema(new DbaseField[0])),
                         result);
                 }
             }
         }
-        private class FakeDbaseRecord : DbaseRecord {}
 
-        private class FakeDbaseSchema : DbaseSchema {}
+        private class FakeDbaseRecord : DbaseRecord
+        {
+            private static readonly FakeDbaseSchema Schema = new FakeDbaseSchema();
+
+            public FakeDbaseRecord()
+            {
+                Field = new DbaseInt32(Schema.Field);
+                Values = new DbaseFieldValue[] { Field };
+            }
+
+            public DbaseInt32 Field { get; }
+        }
+
+        private class FakeDbaseSchema : DbaseSchema
+        {
+            public FakeDbaseSchema()
+            {
+                Fields = new []
+                {
+                    DbaseField.CreateInt32Field(
+                        new DbaseFieldName(nameof(Field)),
+                        new DbaseFieldLength(10))
+                };
+            }
+
+            public DbaseField Field => Fields[0];
+        }
+
         private class FakeDbaseRecordValidator : IZipArchiveDbaseRecordsValidator<FakeDbaseRecord>
         {
             public ZipArchiveErrors Validate(ZipArchiveEntry entry, IEnumerator<FakeDbaseRecord> records)
@@ -99,11 +212,30 @@ namespace RoadRegistry.BackOffice.Translation
             {
                 if (expected == null && actual == null) return true;
                 if (expected == null || actual == null) return false;
+                return expected.Reason.Equals(actual.Reason) &&
+                       expected.Parameters.SequenceEqual(actual.Parameters, new ProblemParameterComparer());
+
             }
 
             public int GetHashCode(Error obj)
             {
-                throw new NotImplementedException();
+                return obj.GetHashCode();
+            }
+        }
+
+        private class ProblemParameterComparer : IEqualityComparer<ProblemParameter>
+        {
+            public bool Equals(ProblemParameter expected, ProblemParameter actual)
+            {
+                if (expected == null && actual == null) return true;
+                if (expected == null || actual == null) return false;
+                return expected.Name.Equals(actual.Name) &&
+                       actual.Value.Contains(expected.Value);
+            }
+
+            public int GetHashCode(ProblemParameter obj)
+            {
+                return obj.GetHashCode();
             }
         }
     }
