161489221c99372435b5d235114d9541d1ef3a01
yreynhout <yves.reynhout@gmail.com>
2020-05-04 12:45:37 +0200

    fix: no swagger docs on startup - overwrite config

--- src/RoadRegistry.BackOffice.UI/init.sh
+++ src/RoadRegistry.BackOffice.UI/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "window.wegenregisterVersion=\"$API_VERSION\";" >> /usr/share/nginx/html/config.f84e1103.js
+echo "window.wegenregisterVersion=\"$API_VERSION\";" > /usr/share/nginx/html/config.f84e1103.js
 echo "window.wegenregisterApiEndpoint=\"$API_ENDPOINT\";" >> /usr/share/nginx/html/config.f84e1103.js
 
 nginx -g 'daemon off;'
