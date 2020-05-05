8f53ac7b43b210c595ce3aed1bdc85012d0644c6
Koen Metsu <koen@metsu.be>
2020-05-05 10:32:12 +0200

    chore: remove irrelevant types from product schema

--- src/RoadRegistry.Product.Schema/RoadNetworkChangesBasedOnArchiveAcceptedEntry.cs
+++
@@ -1,9 +0,0 @@
-namespace RoadRegistry.BackOffice.Schema
-{
-    public class RoadNetworkChangesBasedOnArchiveAcceptedEntry
-    {
-        public RoadNetworkChangesArchiveInfo Archive { get; set; }
-
-        public RoadNetworkAcceptedChange[] Changes { get; set; }
-    }
-}
