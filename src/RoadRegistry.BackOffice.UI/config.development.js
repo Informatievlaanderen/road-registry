4fd99339227467bc429ac5590bad9be2566dc627
yreynhout <yves.reynhout@gmail.com>
2020-05-06 15:32:06 +0200

    fix: ability to run ui live with development config

---
+++ src/RoadRegistry.BackOffice.UI/config.development.js
@@ -0,0 +1,3 @@
+// A placeholder file so we don't get a 404 when running on devServer
+window.wegenregisterVersion="0.0.0";
+window.wegenregisterApiEndpoint="http://localhost:5020";
--- src/RoadRegistry.BackOffice.UI/init.sh
+++ src/RoadRegistry.BackOffice.UI/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "" > /usr/share/nginx/html/config.development.6cf2d3b2.js
+
 echo "window.wegenregisterVersion=\"$API_VERSION\";" > /usr/share/nginx/html/config.f84e1103.js
 echo "window.wegenregisterApiEndpoint=\"$API_ENDPOINT\";" >> /usr/share/nginx/html/config.f84e1103.js
 
