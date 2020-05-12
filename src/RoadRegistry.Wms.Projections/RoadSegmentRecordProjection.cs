3989db26d84a928b8e3486f60212860c352152d7
Koen Metsu <koen@metsu.be>
2020-05-12 14:13:24 +0200

    refactor: remove unnecessary RecyclableMemoryStreamManager

--- src/RoadRegistry.Wms.Projections/RoadSegmentRecordProjection.cs
+++ src/RoadRegistry.Wms.Projections/RoadSegmentRecordProjection.cs
@@ -6,15 +6,13 @@ namespace RoadRegistry.Wms.Projections
     using BackOffice.Messages;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore;
-    using Microsoft.IO;
     using Schema;
     using Schema.RoadSegmentDenorm;
 
     public class RoadSegmentRecordProjection : ConnectedProjection<WmsContext>
     {
-        public RoadSegmentRecordProjection(RecyclableMemoryStreamManager manager, Encoding encoding)
+        public RoadSegmentRecordProjection(Encoding encoding)
         {
-            if (manager == null) throw new ArgumentNullException(nameof(manager));
             if (encoding == null) throw new ArgumentNullException(nameof(encoding));
 
             When<Envelope<ImportedRoadSegment>>(async (context, envelope, token) =>
