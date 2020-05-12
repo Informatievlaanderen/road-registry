8da4d43d1a6a61ebd14423c7d4513f6e608d5f03
Koen Metsu <koen@metsu.be>
2020-05-12 13:18:22 +0200

    test: add example event and denorm expected values from legacy db

--- src/RoadRegistry.Wms.Projections/RoadSegmentRecordProjection.cs
+++ src/RoadRegistry.Wms.Projections/RoadSegmentRecordProjection.cs
@@ -2,6 +2,7 @@ namespace RoadRegistry.Wms.Projections
 {
     using System;
     using System.Text;
+    using BackOffice;
     using BackOffice.Messages;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore;
@@ -18,10 +19,71 @@ namespace RoadRegistry.Wms.Projections
 
             When<Envelope<ImportedRoadSegment>>(async (context, envelope, token) =>
             {
+                var roadSegmentGeometryDrawMethod =
+                    RoadSegmentGeometryDrawMethod.Parse(envelope.Message.GeometryDrawMethod);
 
-                await context.RoadSegments.AddAsync(new RoadSegmentDenormRecord()
+                var accessRestriction =
+                    RoadSegmentAccessRestriction.Parse(envelope.Message.AccessRestriction);
+
+                var roadSegmentStatus = RoadSegmentStatus.Parse(envelope.Message.Status);
+
+                var roadSegmentMorphology = RoadSegmentMorphology.Parse(envelope.Message.Morphology);
+
+                var roadSegmentCategory = RoadSegmentCategory.Parse(envelope.Message.Category);
+
+                await context.RoadSegments.AddAsync(new RoadSegmentDenormRecord
                 {
                     Id = envelope.Message.Id,
+                    BeginOperator = OrganizationId.Unknown,
+                    BeginOrganization = envelope.Message.Origin.OrganizationId,
+                    BeginTime = envelope.Message.Origin.Since,
+                    BeginApplication = "-8",
+
+                    Maintainer = envelope.Message.MaintenanceAuthority.Code,
+                    MaintainerLabel = envelope.Message.MaintenanceAuthority.Name,
+
+                    Method = roadSegmentGeometryDrawMethod.Translation.Identifier,
+                    MethodLabel = roadSegmentGeometryDrawMethod.Translation.Name,
+
+                    Category = roadSegmentCategory.Translation.Identifier,
+                    CategoryLabel = roadSegmentCategory.Translation.Name,
+
+                    Geometry = null,
+                    Geometry2D = null,
+                    GeometryVersion = envelope.Message.GeometryVersion,
+
+                    Morphology = roadSegmentMorphology.Translation.Identifier,
+                    MorphologyLabel = roadSegmentMorphology.Translation.Name,
+
+                    Status = roadSegmentStatus.Translation.Identifier,
+                    StatusLabel = roadSegmentStatus.Translation.Name,
+
+                    AccessRestriction = accessRestriction.Translation.Identifier,
+                    AccessRestrictionLabel = accessRestriction.Translation.Name,
+
+                    SourceId = null,
+                    SourceIdSource = null,
+
+                    OrganizationLabel = envelope.Message.Origin.Organization,
+                    RecordingDate = envelope.Message.RecordingDate,
+                    TransactionId = 0,
+
+                    LeftSideMunicipality = 0,
+                    LeftSideStreetNameId = envelope.Message.LeftSide.StreetNameId,
+                    LeftSideStreetNameLabel = string.IsNullOrWhiteSpace(envelope.Message.LeftSide.StreetName)
+                        ? null
+                        : envelope.Message.LeftSide.StreetName,
+
+                    RightSideMunicipality = 0,
+                    RightSideStreetNameId = envelope.Message.RightSide.StreetNameId,
+                    RightSideStreetNameLabel = string.IsNullOrWhiteSpace(envelope.Message.RightSide.StreetName)
+                        ? null
+                        : envelope.Message.RightSide.StreetName,
+
+                    RoadSegmentVersion = envelope.Message.Version,
+
+                    BeginRoadNodeId = envelope.Message.StartNodeId,
+                    EndRoadNodeId = envelope.Message.EndNodeId
                 }, token);
             });
         }
--- src/RoadRegistry.Wms.Schema/RoadSegmentDenorm/RoadSegmentDenormRecord.cs
+++ src/RoadRegistry.Wms.Schema/RoadSegmentDenorm/RoadSegmentDenormRecord.cs
@@ -1,14 +1,71 @@
 namespace RoadRegistry.Wms.Schema.RoadSegmentDenorm
 {
     using System;
+    using NetTopologySuite.Geometries;
 
     public class RoadSegmentDenormRecord
     {
-        public int Id { get; set; }
-        public int Method { get; set; }
+        public int? Id { get; set; }
+        public int? Method { get; set; }
         public string Maintainer { get; set; }
-        public DateTime BeginTime { get; set; }
+        public DateTime? BeginTime { get; set; }
         public string BeginOperator { get; set; }
         public string BeginOrganization { get; set; }
+
+        public string BeginApplication { get; set; }
+
+        public Geometry Geometry { get; set; }
+
+        public int? Morphology { get; set; }
+
+        public int? Status { get; set; }
+
+        public string Category { get; set; }
+
+        public int? BeginRoadNodeId { get; set; }
+
+        public int? EndRoadNodeId { get; set; }
+
+        public int? LeftSideStreetNameId { get; set; }
+
+        public int? RightSideStreetNameId { get; set; }
+
+        public int? RoadSegmentVersion { get; set; }
+
+        public int? GeometryVersion { get; set; }
+
+        public DateTime RecordingDate { get; set; }
+
+        public int? AccessRestriction { get; set; }
+
+        public int? TransactionId { get; set; }
+
+        public int? SourceId { get; set; }
+
+        public string SourceIdSource { get; set; }
+
+        public int? LeftSideMunicipality { get; set; }
+
+        public int? RightSideMunicipality { get; set; }
+
+        public string CategoryLabel { get; set; }
+
+        public string MethodLabel { get; set; }
+
+        public string MorphologyLabel { get; set; }
+
+        public string AccessRestrictionLabel { get; set; }
+
+        public string StatusLabel { get; set; }
+
+        public string OrganizationLabel { get; set; }
+
+        public string LeftSideStreetNameLabel { get; set; }
+
+        public string RightSideStreetNameLabel { get; set; }
+
+        public string MaintainerLabel { get; set; }
+
+        public Geometry Geometry2D { get; set; }
     }
 }
