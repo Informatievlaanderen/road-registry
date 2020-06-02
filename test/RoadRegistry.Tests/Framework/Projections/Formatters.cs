77d1f6294bf2770134670e5a1b14ba0b2345970a
Koen Metsu <koen@metsu.be>
2020-06-02 15:02:35 +0200

    test: add converters for serializing nts types

--- test/RoadRegistry.Tests/Framework/Projections/Formatters.cs
+++ test/RoadRegistry.Tests/Framework/Projections/Formatters.cs
@@ -1,11 +1,24 @@
 namespace RoadRegistry.Framework.Projections
 {
+    using NetTopologySuite.IO.Converters;
     using Newtonsoft.Json;
 
-    public static class Formatters {
+    public static class Formatters
+    {
+        private static readonly JsonConverter[] Converters;
+
+        static Formatters()
+        {
+            Converters = new JsonConverter[]
+            {
+                new GeometryConverter(),
+                new CoordinateConverter(),
+            };
+        }
+
         public static string NamedJsonMessage<T>(T message)
         {
-            return $"{message.GetType().Name} - {JsonConvert.SerializeObject(message, Formatting.Indented)}";
+            return $"{message.GetType().Name} - {JsonConvert.SerializeObject(message, Formatting.Indented, Converters)}";
         }
     }
 }
