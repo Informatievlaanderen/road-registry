930b7e414a04a53b77c2f319b2dd7934550771d6
Koen Metsu <koen@metsu.be>
2020-07-07 13:50:05 +0200

    feat: add Municipality table to SyndicationContext

---
+++ src/RoadRegistry.Syndication.Schema/MunicipalityConfiguration.cs
@@ -0,0 +1,30 @@
+namespace RoadRegistry.Syndication.Schema
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class MunicipalityConfiguration : IEntityTypeConfiguration<MunicipalityRecord>
+    {
+        public const string TableName = "Municipality";
+
+        public void Configure(EntityTypeBuilder<MunicipalityRecord> b)
+        {
+            b.ToTable(TableName, WellknownSchemas.SyndicationSchema)
+                .HasIndex(p => p.MunicipalityId)
+                .IsClustered(false);
+
+            b.HasKey(p => p.MunicipalityId);
+
+            b.Property(p => p.MunicipalityId)
+                .ValueGeneratedNever()
+                .IsRequired();
+
+            b.Property(p => p.NisCode);
+            b.Property(p => p.DutchName);
+            b.Property(p => p.FrenchName);
+            b.Property(p => p.GermanName);
+            b.Property(p => p.EnglishName);
+            b.Property(p => p.MunicipalityStatus);
+        }
+    }
+}
