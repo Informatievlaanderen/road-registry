be4c33802bfce0d939c1fdc64a356b8e1f03c311
Koen Metsu <koen@metsu.be>
2020-11-11 10:48:53 +0100

    feat: apply Modified/Removed events to RoadSegmentSurfacteAttribute projections

--- src/RoadRegistry.Editor.Projections/RoadSegmentLaneAttributeRecordDbSetExtensions.cs
+++ src/RoadRegistry.Editor.Projections/RoadSegmentLaneAttributeRecordDbSetExtensions.cs
@@ -5,14 +5,13 @@ namespace RoadRegistry.Editor.Projections
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
-    using Schema.RoadSegments;
 
     public static class RoadSegmentLaneAttributeRecordDbSetExtensions
     {
-        public static async Task Synchronize(this DbSet<RoadSegmentLaneAttributeRecord> source,
-            Dictionary<int, RoadSegmentLaneAttributeRecord> currentSet,
-            Dictionary<int, RoadSegmentLaneAttributeRecord> nextSet,
-            Action<RoadSegmentLaneAttributeRecord, RoadSegmentLaneAttributeRecord> action)
+        public static async Task Synchronize<T>(this DbSet<T> source,
+            Dictionary<int, T> currentSet,
+            Dictionary<int, T> nextSet,
+            Action<T, T> action) where T : class
         {
             var newRecordIds = nextSet.Keys.Where(key => !currentSet.ContainsKey(key));
             var removedRecordIds = currentSet.Keys.Where(key => !nextSet.ContainsKey(key));
