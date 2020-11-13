ec7a86a16d2568b47fa23c0325f0037130d05ec2
Koen Metsu <koen@metsu.be>
2020-11-13 11:55:00 +0100

    feat: be more explicit in RoadNetworkInfoSegmentCacheConfiguration

--- src/RoadRegistry.Editor.Schema/RoadNetworkInfoSegmentCacheConfiguration.cs
+++ src/RoadRegistry.Editor.Schema/RoadNetworkInfoSegmentCacheConfiguration.cs
@@ -15,6 +15,14 @@ namespace RoadRegistry.Editor.Schema
 
             b.HasKey(p => p.RoadSegmentId);
             b.Property(p => p.RoadSegmentId).ValueGeneratedNever().IsRequired();
+
+            b.Property(p => p.SurfacesLength);
+            b.Property(p => p.LanesLength);
+            b.Property(p => p.ShapeLength);
+            b.Property(p => p.WidthsLength);
+            b.Property(p => p.PartOfEuropeanRoadsLength);
+            b.Property(p => p.PartOfNationalRoadsLength);
+            b.Property(p => p.PartOfNumberedRoadsLength);
         }
     }
 }
