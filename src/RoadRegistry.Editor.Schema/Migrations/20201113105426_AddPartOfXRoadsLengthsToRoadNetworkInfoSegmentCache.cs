c13e750973496a87702627d6046710e4c83c24d1
Koen Metsu <koen@metsu.be>
2020-11-13 11:55:30 +0100

    feat: add migration for PartOfXRoadsLength properties in RoadNetworkInfoSegmentCache

---
+++ src/RoadRegistry.Editor.Schema/Migrations/20201113105426_AddPartOfXRoadsLengthsToRoadNetworkInfoSegmentCache.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RoadRegistry.Editor.Schema.Migrations
+{
+    public partial class AddPartOfXRoadsLengthsToRoadNetworkInfoSegmentCache : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PartOfEuropeanRoadsLength",
+                schema: "RoadRegistryEditor",
+                table: "RoadNetworkInfoSegmentCache",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PartOfNationalRoadsLength",
+                schema: "RoadRegistryEditor",
+                table: "RoadNetworkInfoSegmentCache",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PartOfNumberedRoadsLength",
+                schema: "RoadRegistryEditor",
+                table: "RoadNetworkInfoSegmentCache",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PartOfEuropeanRoadsLength",
+                schema: "RoadRegistryEditor",
+                table: "RoadNetworkInfoSegmentCache");
+
+            migrationBuilder.DropColumn(
+                name: "PartOfNationalRoadsLength",
+                schema: "RoadRegistryEditor",
+                table: "RoadNetworkInfoSegmentCache");
+
+            migrationBuilder.DropColumn(
+                name: "PartOfNumberedRoadsLength",
+                schema: "RoadRegistryEditor",
+                table: "RoadNetworkInfoSegmentCache");
+        }
+    }
+}
