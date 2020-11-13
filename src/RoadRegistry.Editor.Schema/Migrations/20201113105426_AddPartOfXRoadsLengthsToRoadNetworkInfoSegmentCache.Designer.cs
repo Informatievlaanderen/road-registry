3af2b3db4eae8c94db0afd2604ee9036d0f8700e
Koen Metsu <koen@metsu.be>
2020-11-13 11:55:30 +0100

    feat: add migration for PartOfXRoadsLength properties in RoadNetworkInfoSegmentCache

--- src/RoadRegistry.Editor.Schema/Migrations/20201113105426_AddPartOfXRoadsLengthsToRoadNetworkInfoSegmentCache.Designer.cs
+++ src/RoadRegistry.Editor.Schema/Migrations/20201113105426_AddPartOfXRoadsLengthsToRoadNetworkInfoSegmentCache.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RoadRegistry.Editor.Schema;
 
 namespace RoadRegistry.Editor.Schema.Migrations
 {
     [DbContext(typeof(EditorContext))]
-    partial class EditorContextModelSnapshot : ModelSnapshot
+    [Migration("20201113105426_AddPartOfXRoadsLengthsToRoadNetworkInfoSegmentCache")]
+    partial class AddPartOfXRoadsLengthsToRoadNetworkInfoSegmentCache
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
