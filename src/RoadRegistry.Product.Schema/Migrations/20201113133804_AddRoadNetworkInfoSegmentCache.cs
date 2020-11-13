ef034e722f5510bbcb8f2e6346130470ad435e18
Koen Metsu <koen@metsu.be>
2020-11-13 14:38:37 +0100

    feat: add migration for RoadNetworkInfoSegmentCache

---
+++ src/RoadRegistry.Product.Schema/Migrations/20201113133804_AddRoadNetworkInfoSegmentCache.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RoadRegistry.Product.Schema.Migrations
+{
+    public partial class AddRoadNetworkInfoSegmentCache : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RoadNetworkInfoSegmentCache",
+                schema: "RoadRegistryProduct",
+                columns: table => new
+                {
+                    RoadSegmentId = table.Column<int>(nullable: false),
+                    ShapeLength = table.Column<int>(nullable: false),
+                    SurfacesLength = table.Column<int>(nullable: false),
+                    LanesLength = table.Column<int>(nullable: false),
+                    WidthsLength = table.Column<int>(nullable: false),
+                    PartOfEuropeanRoadsLength = table.Column<int>(nullable: false),
+                    PartOfNationalRoadsLength = table.Column<int>(nullable: false),
+                    PartOfNumberedRoadsLength = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoadNetworkInfoSegmentCache", x => x.RoadSegmentId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoadNetworkInfoSegmentCache_RoadSegmentId",
+                schema: "RoadRegistryProduct",
+                table: "RoadNetworkInfoSegmentCache",
+                column: "RoadSegmentId")
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoadNetworkInfoSegmentCache",
+                schema: "RoadRegistryProduct");
+        }
+    }
+}
