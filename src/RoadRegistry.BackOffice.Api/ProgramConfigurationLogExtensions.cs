f7e492451bb0335e929ef9a64c06a587e116cea8
yreynhout <yves.reynhout@gmail.com>
2021-03-02 11:53:56 +0100

    feat: log connectionstring upon api startup

---
+++ src/RoadRegistry.BackOffice.Api/ProgramConfigurationLogExtensions.cs
@@ -0,0 +1,26 @@
+namespace RoadRegistry.BackOffice.Api
+{
+    using System;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    internal static class ProgramConfigurationLogExtensions
+    {
+        public static void LogSqlServerConnectionString(
+            this ILogger<Program> logger,
+            IConfiguration configuration,
+            string connectionName)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+            if (connectionName == null) throw new ArgumentNullException(nameof(connectionName));
+
+            logger.LogInformation("{ConnectionName} connection string set to:{ConnectionString}",
+                connectionName,
+                new SqlConnectionStringBuilder(configuration.GetConnectionString(connectionName))
+                {
+                    Password = "**REDACTED**"
+                }.ConnectionString);
+        }
+    }
+}
