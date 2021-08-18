618400dd02ebde158aabc41239defa525ff19323
yreynhout <yves.reynhout@gmail.com>
2021-08-18 16:20:22 +0200

    fix: always replace instead of conditionally

--- src/RoadRegistry.BackOffice/Core/MemoryRoadNetworkSnapshotReaderWriter.cs
+++ src/RoadRegistry.BackOffice/Core/MemoryRoadNetworkSnapshotReaderWriter.cs
@@ -1,6 +1,7 @@
 namespace RoadRegistry.BackOffice.Core
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Threading;
     using System.Threading.Tasks;
     using Messages;
@@ -40,9 +41,60 @@ namespace RoadRegistry.BackOffice.Core
 
         public Task WriteSnapshot(RoadNetworkSnapshot snapshot, int version, CancellationToken cancellationToken)
         {
-            Interlocked.CompareExchange(ref _cache,
-                new CachedRoadNetworkSnapshot { Snapshot = snapshot, Version = version },
-                null);
+            Interlocked.Exchange(ref _cache,
+                new CachedRoadNetworkSnapshot { Snapshot = snapshot, Version = version });
+            return Task.CompletedTask;
+        }
+
+        private class CachedRoadNetworkSnapshot
+        {
+            public RoadNetworkSnapshot Snapshot { get; set; }
+            public int Version { get; set; }
+        }
+    }
+
+    public class Memory2RoadNetworkSnapshotReaderWriter : IRoadNetworkSnapshotReader, IRoadNetworkSnapshotWriter
+    {
+        private readonly IRoadNetworkSnapshotReader _fallbackReader;
+        private readonly ConcurrentDictionary<int, CachedRoadNetworkSnapshot> _cache;
+
+        // NOTE: For reading we fallback to another snapshot reader
+        // For writing we don't since we don't want to pay serialization and I/O tax nor do we want to deal with possible race conditions (depending on how the roadnetwork command handling is hosted)
+        // Writing new snapshots is still done asynchronously, by a single thread in a single process.
+
+        public Memory2RoadNetworkSnapshotReaderWriter(IRoadNetworkSnapshotReader fallbackReader)
+        {
+            _fallbackReader = fallbackReader ?? throw new ArgumentNullException(nameof(fallbackReader));
+            _cache = new ConcurrentDictionary<int, CachedRoadNetworkSnapshot>();
+        }
+        public async Task<(RoadNetworkSnapshot snapshot, int version)> ReadSnapshot(CancellationToken cancellationToken)
+        {
+            _cache.T
+            if (_cache.TryPeek(out var cached))
+            {
+                return (cached.Snapshot, cached.Version);
+            }
+
+            var (snapshot, version) = await _fallbackReader.ReadSnapshot(cancellationToken);
+            if (snapshot != null)
+            {
+                _cache.Add(new CachedRoadNetworkSnapshot { Snapshot = snapshot, Version = version });
+                foreach (var item in _cache.ToArray())
+                {
+                    _cache.R
+                }
+                Interlocked.CompareExchange(
+                    ref _cache,
+                    new CachedRoadNetworkSnapshot { Snapshot = snapshot, Version = version },
+                    null);
+            }
+            return (snapshot, version);
+        }
+
+        public Task WriteSnapshot(RoadNetworkSnapshot snapshot, int version, CancellationToken cancellationToken)
+        {
+            Interlocked.Exchange(ref _cache,
+                new CachedRoadNetworkSnapshot { Snapshot = snapshot, Version = version });
             return Task.CompletedTask;
         }
 
