bab9975f2f90b0d5ee6007dfb1ab11835e84b150
ChristopheVr <christophe.vroonen@gmail.com>
2021-09-20 11:39:40 +0200

    feat: add support for 'not known' (-8) volgnummer in numbered roads

--- src/RoadRegistry.BackOffice/RoadSegmentNumberedRoadOrdinal.cs
+++ src/RoadRegistry.BackOffice/RoadSegmentNumberedRoadOrdinal.cs
@@ -4,19 +4,24 @@ namespace RoadRegistry.BackOffice
 
     public readonly struct RoadSegmentNumberedRoadOrdinal : IEquatable<RoadSegmentNumberedRoadOrdinal>
     {
+        internal struct WellKnownValues
+        {
+            public const int NotKnown = -8;
+        }
+
         private readonly int _value;
 
         public RoadSegmentNumberedRoadOrdinal(int value)
         {
-            if (value < 0)
-                throw new ArgumentOutOfRangeException(nameof(value), value, "The road segment numbered road ordinal must be greater than or equal to zero.");
+            if (value < 0 && value != WellKnownValues.NotKnown)
+                throw new ArgumentOutOfRangeException(nameof(value), value, $"The road segment numbered road ordinal must be greater than or equal to zero, or the value '{WellKnownValues.NotKnown}' (Not Known)");
 
             _value = value;
         }
 
         public static bool Accepts(int value)
         {
-            return value >= 0;
+            return value >= 0 || value == WellKnownValues.NotKnown;
         }
 
         public int ToInt32() => _value;
