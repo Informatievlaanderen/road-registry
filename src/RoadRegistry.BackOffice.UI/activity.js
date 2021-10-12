51c7a6bd84a182be5b0befa63eda949a614bc885
ChristopheVr <christophe.vroonen@gmail.com>
2021-10-12 09:31:06 +0200

    fix: fix wrong parameters

--- src/RoadRegistry.BackOffice.UI/activity.js
+++ src/RoadRegistry.BackOffice.UI/activity.js
@@ -3,7 +3,7 @@ import { Elm } from './output/activity.js'
 Elm.Activity.init({
 	node: document.getElementById('app'),
   flags: {
-    endpoint: 'http://localhost:2080',
-    apikey: '00000000-0000-0000-0000-000000000000'
+    endpoint: window.wegenregisterApiEndpoint,
+    apikey: window.wegenregisterApiKey
   }
 });
