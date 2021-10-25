b0fda9d27f080d71ec703758be1eb15411c3613c
ChristopheVr <christophe.vroonen@gmail.com>
2021-10-25 13:33:27 +0200

    feat: add endpoint to create extract by contour

---
+++ src/RoadRegistry.BackOffice.Api/Extracts/DownloadExtractByContourRequestBodyValidator.cs
@@ -0,0 +1,45 @@
+namespace RoadRegistry.BackOffice.Api.Extracts
+{
+    using System;
+    using FluentValidation;
+    using Microsoft.Extensions.Logging;
+    using NetTopologySuite.Geometries;
+    using NetTopologySuite.IO;
+
+    public class DownloadExtractByContourRequestBodyValidator : AbstractValidator<DownloadExtractByContourRequestBody>
+    {
+        private readonly WKTReader _reader;
+        private readonly ILogger<DownloadExtractByContourRequestBodyValidator> _logger;
+
+        public DownloadExtractByContourRequestBodyValidator(WKTReader reader, ILogger<DownloadExtractByContourRequestBodyValidator> logger)
+        {
+            _reader = reader ?? throw new ArgumentNullException(nameof(reader));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            RuleFor(c => c.Contour)
+                .NotEmpty().WithMessage("'Contour' must not be empty, null or missing")
+                .Must(BeMultiPolygonGeometryAsWellKnownText)
+                .WithMessage("'Contour' must be a valid multipolygon or polygon represented as well-known text")
+                .When(c => !string.IsNullOrEmpty(c.Contour), ApplyConditionTo.CurrentValidator);
+        }
+
+        private bool BeMultiPolygonGeometryAsWellKnownText(string text)
+        {
+            try
+            {
+                var geometry = _reader.Read(text);
+                return geometry switch
+                {
+                    MultiPolygon multiPolygon => multiPolygon.IsValid,
+                    Polygon polygon => polygon.IsValid,
+                    _ => false
+                };
+            }
+            catch (ParseException exception)
+            {
+                _logger.LogWarning(exception, "The download extract request body validation encountered a problem while trying to parse the contour as well-known text");
+                return false;
+            }
+        }
+    }
+}
