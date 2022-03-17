0286b4f3fa43675a28379244a6f5b0a4d825c108
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2022-03-17 12:53:17 +0100

    fix: remove empty new-line

--- run.sh
+++ run.sh
@@ -2,4 +2,3 @@
 set -e
 
 docker-compose up --build municipality-api streetname-api syndication-projection-host editor-projection-host product-projection-host wms-projection-host backoffice-event-host backoffice-extract-host backoffice-command-host backoffice-api backoffice-ui
-
