117c18290608abdeb7b260ce61107f62315c1f83
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2022-05-24 15:46:10 +0200

    fix: add vue comp and migrate upload page

--- src/RoadRegistry.BackOffice.UI/src/core/components/index.ts
+++ src/RoadRegistry.BackOffice.UI/src/core/components/index.ts
@@ -1,6 +1,22 @@
 import Vue from "vue";
 import Header from "./Header.vue";
 import Footer from "./Footer.vue";
+const VlUiVueComponents = require("@govflanders/vl-ui-vue-components");
+const { VlModalToggle } = require("@govflanders/vl-ui-vue-components");
+
 
 Vue.component("wr-header", Header);
 Vue.component("wr-footer", Footer);
+
+// configuration of the built-in validator
+const validatorConfig = {
+    inject: true,
+    locale: "nl",
+  };
+
+// install the component library with config
+Vue.use(VlUiVueComponents, {
+    validation: validatorConfig,
+});
+  
+Vue.directive("vl-modal-toggle", VlModalToggle);
