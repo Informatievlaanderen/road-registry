8da21a9a13936bdad74c4f0a7c72c2c4ed7527be
Jan Lesage <jan.lesage@vlaanderen.be>
2022-06-13 15:22:05 +0200

    Typography

--- src/RoadRegistry.BackOffice.UI/src/auth/views/Logout.vue
+++ src/RoadRegistry.BackOffice.UI/src/auth/views/Logout.vue
@@ -1,5 +1,17 @@
 <template>
-<h1>Afmelden</h1>
+    <div>
+        <div class="vl-typography">
+            <h2>Afmelden</h2>
+        </div>
+
+        <div class="vl-form-grid vl-form-grid--is-stacked">
+            <div class="vl-form-col--12-12">
+                <vl-alert icon="warning" title="Opgelet!" mod-warning role="alertdialog">
+                    <p>U bent afgemeld van onze diensten.</p>
+                </vl-alert>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
