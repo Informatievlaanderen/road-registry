50645c2dc98060d3173df09de024a7a3ba2653aa
Jan Lesage <jan.lesage@vlaanderen.be>
2022-08-26 16:04:19 +0200

    feat: Support multiple versions for transactiezones.dbf

--- src/RoadRegistry.BackOffice/Uploads/Schema/TransactionZoneDbaseRecord.cs
+++ src/RoadRegistry.BackOffice/Uploads/Schema/TransactionZoneDbaseRecord.cs
@@ -2,32 +2,65 @@ namespace RoadRegistry.BackOffice.Uploads.Schema
 {
     using Be.Vlaanderen.Basisregisters.Shaperon;
 
-    public class TransactionZoneDbaseRecord : DbaseRecord
+    namespace V1
     {
-        public static readonly TransactionZoneDbaseSchema Schema = new TransactionZoneDbaseSchema();
-
-        public TransactionZoneDbaseRecord()
+        public class TransactionZoneDbaseRecord : DbaseRecord
         {
-            SOURCEID = new DbaseInt32(Schema.SOURCEID);
-            TYPE = new DbaseInt32(Schema.TYPE);
-            BESCHRIJV = new DbaseString(Schema.BESCHRIJV);
-            OPERATOR = new DbaseString(Schema.OPERATOR);
-            ORG = new DbaseString(Schema.ORG);
-            APPLICATIE = new DbaseString(Schema.APPLICATIE);
-            DOWNLOADID = new DbaseString(Schema.DOWNLOADID);
+            public static readonly TransactionZoneDbaseSchema Schema = new TransactionZoneDbaseSchema();
 
-            Values = new DbaseFieldValue[]
+            public TransactionZoneDbaseRecord()
             {
-                SOURCEID, TYPE, BESCHRIJV, OPERATOR, ORG, APPLICATIE, DOWNLOADID
-            };
-        }
+                SOURCEID = new DbaseInt32(Schema.SOURCEID);
+                TYPE = new DbaseInt32(Schema.TYPE);
+                BESCHRIJV = new DbaseString(Schema.BESCHRIJV);
+                OPERATOR = new DbaseString(Schema.OPERATOR);
+                ORG = new DbaseString(Schema.ORG);
+                APPLICATIE = new DbaseString(Schema.APPLICATIE);
 
-        public DbaseInt32 SOURCEID { get; }
-        public DbaseInt32 TYPE { get; }
-        public DbaseString BESCHRIJV { get; }
-        public DbaseString OPERATOR { get; }
-        public DbaseString ORG { get; }
-        public DbaseString APPLICATIE { get; }
-        public DbaseString DOWNLOADID { get; }
+                Values = new DbaseFieldValue[]
+                {
+                    SOURCEID, TYPE, BESCHRIJV, OPERATOR, ORG, APPLICATIE
+                };
+            }
+
+            public DbaseInt32 SOURCEID { get; }
+            public DbaseInt32 TYPE { get; }
+            public DbaseString BESCHRIJV { get; }
+            public DbaseString OPERATOR { get; }
+            public DbaseString ORG { get; }
+            public DbaseString APPLICATIE { get; }
+        }
+    }
+
+    namespace V2
+    {
+        public class TransactionZoneDbaseRecord : DbaseRecord
+        {
+            public static readonly TransactionZoneDbaseSchema Schema = new TransactionZoneDbaseSchema();
+
+            public TransactionZoneDbaseRecord()
+            {
+                SOURCEID = new DbaseInt32(Schema.SOURCEID);
+                TYPE = new DbaseInt32(Schema.TYPE);
+                BESCHRIJV = new DbaseString(Schema.BESCHRIJV);
+                OPERATOR = new DbaseString(Schema.OPERATOR);
+                ORG = new DbaseString(Schema.ORG);
+                APPLICATIE = new DbaseString(Schema.APPLICATIE);
+                DOWNLOADID = new DbaseString(Schema.DOWNLOADID);
+
+                Values = new DbaseFieldValue[]
+                {
+                SOURCEID, TYPE, BESCHRIJV, OPERATOR, ORG, APPLICATIE, DOWNLOADID
+                };
+            }
+
+            public DbaseInt32 SOURCEID { get; }
+            public DbaseInt32 TYPE { get; }
+            public DbaseString BESCHRIJV { get; }
+            public DbaseString OPERATOR { get; }
+            public DbaseString ORG { get; }
+            public DbaseString APPLICATIE { get; }
+            public DbaseString DOWNLOADID { get; }
+        }
     }
 }
--- src/RoadRegistry.BackOffice/Uploads/TransactionZoneDbaseRecordsTranslator.cs
+++ src/RoadRegistry.BackOffice/Uploads/TransactionZoneDbaseRecordsTranslator.cs
@@ -1,28 +1,54 @@
 namespace RoadRegistry.BackOffice.Uploads
 {
+    using Be.Vlaanderen.Basisregisters.Shaperon;
     using System;
     using System.IO.Compression;
-    using Be.Vlaanderen.Basisregisters.Shaperon;
-    using Schema;
 
-    public class TransactionZoneDbaseRecordsTranslator : IZipArchiveDbaseRecordsTranslator<TransactionZoneDbaseRecord>
+    namespace Schema.V1
     {
-        public TranslatedChanges Translate(ZipArchiveEntry entry, IDbaseRecordEnumerator<TransactionZoneDbaseRecord> records, TranslatedChanges changes)
+        public class TransactionZoneDbaseRecordsTranslator : IZipArchiveDbaseRecordsTranslator<TransactionZoneDbaseRecord>
         {
-            if (entry == null) throw new ArgumentNullException(nameof(entry));
-            if (records == null) throw new ArgumentNullException(nameof(records));
-            if (changes == null) throw new ArgumentNullException(nameof(changes));
-
-            if (records.MoveNext() && records.Current != null)
+            public TranslatedChanges Translate(ZipArchiveEntry entry, IDbaseRecordEnumerator<TransactionZoneDbaseRecord> records, TranslatedChanges changes)
             {
-                return changes
-                    .WithReason(new Reason(records.Current.BESCHRIJV.Value))
-                    .WithOperatorName(string.IsNullOrEmpty(records.Current.OPERATOR.Value)
-                        ? new OperatorName("-8")
-                        : new OperatorName(records.Current.OPERATOR.Value))
-                    .WithOrganization(new OrganizationId(records.Current.ORG.Value));
+                if (entry == null) throw new ArgumentNullException(nameof(entry));
+                if (records == null) throw new ArgumentNullException(nameof(records));
+                if (changes == null) throw new ArgumentNullException(nameof(changes));
+
+                if (records.MoveNext() && records.Current != null)
+                {
+                    return changes
+                        .WithReason(new Reason(records.Current.BESCHRIJV.Value))
+                        .WithOperatorName(string.IsNullOrEmpty(records.Current.OPERATOR.Value)
+                            ? new OperatorName("-8")
+                            : new OperatorName(records.Current.OPERATOR.Value))
+                        .WithOrganization(new OrganizationId(records.Current.ORG.Value));
+                }
+                return changes;
             }
-            return changes;
+        }
+    }
+
+    namespace Schema.V2
+    {
+        public class TransactionZoneDbaseRecordsTranslator : IZipArchiveDbaseRecordsTranslator<TransactionZoneDbaseRecord>
+        {
+            public TranslatedChanges Translate(ZipArchiveEntry entry, IDbaseRecordEnumerator<TransactionZoneDbaseRecord> records, TranslatedChanges changes)
+            {
+                if (entry == null) throw new ArgumentNullException(nameof(entry));
+                if (records == null) throw new ArgumentNullException(nameof(records));
+                if (changes == null) throw new ArgumentNullException(nameof(changes));
+
+                if (records.MoveNext() && records.Current != null)
+                {
+                    return changes
+                        .WithReason(new Reason(records.Current.BESCHRIJV.Value))
+                        .WithOperatorName(string.IsNullOrEmpty(records.Current.OPERATOR.Value)
+                            ? new OperatorName("-8")
+                            : new OperatorName(records.Current.OPERATOR.Value))
+                        .WithOrganization(new OrganizationId(records.Current.ORG.Value));
+                }
+                return changes;
+            }
         }
     }
 }
