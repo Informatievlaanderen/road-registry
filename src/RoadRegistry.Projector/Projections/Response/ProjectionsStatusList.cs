884388af292af76def1d6908ca061a4bc8b573fe
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2022-09-12 13:22:10 +0200

    feat: create projector service for status page

---
+++ src/RoadRegistry.Projector/Projections/Response/ProjectionsStatusList.cs
@@ -0,0 +1,20 @@
+namespace RoadRegistry.Projector.Projections.Response
+{
+    using System.Collections.Generic;
+
+    public class ProjectionsStatusList
+    {
+        public List<ProjectionStatus> Projections { get; set; }
+        public long StreamPosition { get; set; }
+    }
+
+    public class ProjectionStatus
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+        public long CurrentPosition { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
--- src/RoadRegistry.Wfs.Schema/MigrationTables.cs
+++
@@ -1,7 +0,0 @@
-namespace RoadRegistry.Wfs.Schema
-{
-    public static class MigrationTables
-    {
-        public const string Wfs = "__EFMigrationsHistoryWfs";
-    }
-}
