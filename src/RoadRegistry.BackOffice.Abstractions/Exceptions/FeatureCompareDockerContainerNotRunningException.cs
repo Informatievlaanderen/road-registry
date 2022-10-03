27b041bb2c739ae808cdef44679bc4250aa5e894
Rik De Peuter <rikdepeuter@gmail.com>
2022-10-03 16:25:35 +0200

    fix: cleanup code

--- src/RoadRegistry.BackOffice.Abstractions/Exceptions/FeatureCompareDockerContainerNotRunningException.cs
+++ src/RoadRegistry.BackOffice.Abstractions/Exceptions/FeatureCompareDockerContainerNotRunningException.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace RoadRegistry.BackOffice.Abstractions.Exceptions;
 
-namespace RoadRegistry.BackOffice.Abstractions.Exceptions
+public class FeatureCompareDockerContainerNotRunningException : ApplicationException
 {
-    public class FeatureCompareDockerContainerNotRunningException : ApplicationException
+    public FeatureCompareDockerContainerNotRunningException(string message, ArchiveId archiveId) : base(message)
     {
-        public FeatureCompareDockerContainerNotRunningException(string message, ArchiveId archiveId) : base(message)
-        {
-            ArchiveId = archiveId;
-        }
-
-        public ArchiveId ArchiveId { get; init; }
+        ArchiveId = archiveId;
     }
+
+    public ArchiveId ArchiveId { get; init; }
 }
