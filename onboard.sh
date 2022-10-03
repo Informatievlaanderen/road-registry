9230a3bcd88b102ef08b12f3e8050585acb779f7
Rik De Peuter <rikdepeuter@gmail.com>
2022-10-03 13:15:45 +0200

    build: remove onboard.sh

--- onboard.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-aws sso login --profile vbr-test
-
-curl 'https://basisregisters-local-dev.s3.eu-west-1.amazonaws.com/database_backup.zip'
-
-curl 'https://basisregisters-local-dev.s3.eu-west-1.amazonaws.com/legacy_db_filled.tar.gz'
-tar -xvf legacy_db_filled.tar.gz
