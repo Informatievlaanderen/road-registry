81028919571aebf6d6f481dd9cdec1b78b169779
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-10-19 14:09:34 +0200

    fix: conform to serializable pattern

--- src/RoadRegistry.BackOffice.Abstractions/Exceptions/ExtractDownloadNotFoundException.cs
+++ src/RoadRegistry.BackOffice.Abstractions/Exceptions/ExtractDownloadNotFoundException.cs
@@ -2,20 +2,18 @@ namespace RoadRegistry.BackOffice.Abstractions.Exceptions;
 
 using System.Runtime.Serialization;
 
-public class ExtractDownloadNotFoundException : ApplicationException
+[Serializable]
+public sealed class ExtractDownloadNotFoundException : ApplicationException
 {
     public ExtractDownloadNotFoundException(DownloadId downloadId)
         : this(downloadId.ToString())
-    {
-    }
+    { }
 
     public ExtractDownloadNotFoundException(string identifier)
         : base($"Could not find the download with identifier {identifier}")
-    {
-    }
+    { }
 
     private ExtractDownloadNotFoundException(SerializationInfo info, StreamingContext context)
         : base(info, context)
-    {
-    }
+    { }
 }
