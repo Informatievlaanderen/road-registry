f9a736ddc479e3a6a00cc2c669827d0d1b36b68b
Jan Lesage <jan.lesage@vlaanderen.be>
2022-10-24 10:45:40 +0200

    feat: WKT validation

--- src/RoadRegistry.BackOffice.Api/Information/InformationController.cs
+++ src/RoadRegistry.BackOffice.Api/Information/InformationController.cs
@@ -1,10 +1,14 @@
 namespace RoadRegistry.BackOffice.Api.Information;
 
+using System.Threading;
 using System.Threading.Tasks;
+using Abstractions.Exceptions;
 using Be.Vlaanderen.Basisregisters.Api;
 using Editor.Schema;
+using FluentValidation;
 using Infrastructure;
 using Infrastructure.Controllers.Attributes;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,11 +20,26 @@ using Microsoft.EntityFrameworkCore;
 [ApiKeyAuth("Road")]
 public class InformationController : ControllerBase
 {
+    private readonly IMediator _mediator;
+
+    public InformationController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
     [HttpGet("")]
     public async Task<IActionResult> Get([FromServices] EditorContext context)
     {
         var info = await context.RoadNetworkInfo.SingleOrDefaultAsync(HttpContext.RequestAborted);
         if (info == null || !info.CompletedImport) return StatusCode(StatusCodes.Status503ServiceUnavailable);
         return new JsonResult(RoadNetworkInformationResponse.From(info));
+    }
+
+    [HttpPost("wkt-validation")]
+    public async Task<IActionResult> ValidateWKT([FromBody] string wktContour, CancellationToken cancellationToken)
+    {
+        var request = new ValidateWktContourRequest();
+        var response = await _mediator.Send(request, cancellationToken);
+        return new ValidateWktContourResponse(response);
     }
 }
