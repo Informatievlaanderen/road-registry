4e2ff6843118d989c57680a72bce2c4cf8cfbba0
Jan Lesage <jan.lesage@vlaanderen.be>
2022-10-24 14:56:42 +0200

    feat: Geometry translator

--- src/RoadRegistry.BackOffice.Handlers/Extracts/DownloadExtractByFileRequestHandler.cs
+++ src/RoadRegistry.BackOffice.Handlers/Extracts/DownloadExtractByFileRequestHandler.cs
@@ -1,9 +1,15 @@
 namespace RoadRegistry.BackOffice.Handlers.Extracts;
 
+using System.Text;
 using Abstractions;
 using Abstractions.Extracts;
+using Azure.Core;
+using Be.Vlaanderen.Basisregisters.Shaperon;
 using Framework;
+using Messages;
 using Microsoft.Extensions.Logging;
+using Microsoft.IO;
+using NetTopologySuite.Geometries;
 
 public class DownloadExtractByFileRequestHandler : EndpointRequestHandler<DownloadExtractByFileRequest, DownloadExtractByFileResponse>
 {
@@ -17,17 +23,27 @@ public class DownloadExtractByFileRequestHandler : EndpointRequestHandler<Downlo
     {
         var downloadId = new DownloadId(Guid.NewGuid());
         var randomExternalRequestId = Guid.NewGuid().ToString("N");
+        var contour = await GetRoadNetworkExtractGeometryFromShapeAsync(request.ShpFile, request.Buffer, cancellationToken);
 
-        //var message = new Command(
-        //    new RequestRoadNetworkExtract
-        //    {
-        //        ExternalRequestId = randomExternalRequestId,
-        //        Contour = GeometryTranslator.TranslateToRoadNetworkExtractGeometry(_reader.Read(request.Contour) as IPolygonal, request.Buffer),
-        //        DownloadId = downloadId,
-        //        Description = request.Description
-        //    });
-        //await Dispatcher(message, cancellationToken);
+        var message = new Command(
+            new RequestRoadNetworkExtract
+            {
+                ExternalRequestId = randomExternalRequestId,
+                Contour = contour,
+                DownloadId = downloadId,
+                Description = request.Description
+            });
+        await Dispatcher(message, cancellationToken);
 
         return new DownloadExtractByFileResponse(downloadId);
+    }
+
+    private static async Task<RoadNetworkExtractGeometry> GetRoadNetworkExtractGeometryFromShapeAsync(DownloadExtractByFileRequestItem shapeFile, int buffer, CancellationToken cancellationToken)
+    {
+        using var reader = new BinaryReader(shapeFile.ReadStream, Encoding.UTF8);
+        var shapeContent = ShapeContent.Read(reader);
+
+        var result = await Task.FromResult(GeometryTranslator.TranslateToRoadNetworkExtractGeometry(shapeContent as IPolygonal, buffer));
+        return result;
     }
 }
