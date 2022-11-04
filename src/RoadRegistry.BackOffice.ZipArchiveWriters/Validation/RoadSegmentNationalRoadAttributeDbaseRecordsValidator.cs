7bae78b5603ae106b8593961df0b5a40e845ae99
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-11-04 11:48:00 +0100

    fix: remove unused variables

--- src/RoadRegistry.BackOffice.ZipArchiveWriters/Validation/RoadSegmentNationalRoadAttributeDbaseRecordsValidator.cs
+++ src/RoadRegistry.BackOffice.ZipArchiveWriters/Validation/RoadSegmentNationalRoadAttributeDbaseRecordsValidator.cs
@@ -9,16 +9,16 @@ public class RoadSegmentNationalRoadAttributeDbaseRecordsValidator : IZipArchive
 {
     public (ZipArchiveProblems, ZipArchiveValidationContext) Validate(ZipArchiveEntry entry, IDbaseRecordEnumerator<RoadSegmentNationalRoadAttributeDbaseRecord> records, ZipArchiveValidationContext context)
     {
-        if (entry == null) throw new ArgumentNullException(nameof(entry));
-        if (records == null) throw new ArgumentNullException(nameof(records));
-        if (context == null) throw new ArgumentNullException(nameof(context));
+        ArgumentNullException.ThrowIfNull(nameof(entry));
+        ArgumentNullException.ThrowIfNull(nameof(records));
+        ArgumentNullException.ThrowIfNull(nameof(context));
 
         var problems = ZipArchiveProblems.None;
         try
         {
-            var identifiers = new Dictionary<AttributeId, RecordNumber>();
             var moved = records.MoveNext();
             if (moved)
+            {
                 while (moved)
                 {
                     var recordContext = entry.AtDbaseRecord(records.CurrentRecordNumber);
@@ -27,7 +27,10 @@ public class RoadSegmentNationalRoadAttributeDbaseRecordsValidator : IZipArchive
                     {
                         if (record.NW_OIDN.HasValue)
                         {
-                            if (record.NW_OIDN.Value == 0) problems += recordContext.IdentifierZero();
+                            if (record.NW_OIDN.Value == 0)
+                            {
+                                problems += recordContext.IdentifierZero();
+                            }
                         }
                         else
                         {
@@ -35,18 +38,31 @@ public class RoadSegmentNationalRoadAttributeDbaseRecordsValidator : IZipArchive
                         }
 
                         if (!record.IDENT2.HasValue)
+                        {
                             problems += recordContext.RequiredFieldIsNull(record.IDENT2.Field);
-                        else if (!NationalRoadNumber.CanParse(record.IDENT2.Value)) problems += recordContext.NotNationalRoadNumber(record.IDENT2.Value);
+                        }
+                        else if (!NationalRoadNumber.CanParse(record.IDENT2.Value))
+                        {
+                            problems += recordContext.NotNationalRoadNumber(record.IDENT2.Value);
+                        }
 
                         if (!record.WS_OIDN.HasValue)
+                        {
                             problems += recordContext.RequiredFieldIsNull(record.WS_OIDN.Field);
-                        else if (!RoadSegmentId.Accepts(record.WS_OIDN.Value)) problems += recordContext.RoadSegmentIdOutOfRange(record.WS_OIDN.Value);
+                        }
+                        else if (!RoadSegmentId.Accepts(record.WS_OIDN.Value))
+                        {
+                            problems += recordContext.RoadSegmentIdOutOfRange(record.WS_OIDN.Value);
+                        }
 
                         moved = records.MoveNext();
                     }
                 }
+            }
             else
+            {
                 problems += entry.HasNoDbaseRecords(true);
+            }
         }
         catch (Exception exception)
         {
