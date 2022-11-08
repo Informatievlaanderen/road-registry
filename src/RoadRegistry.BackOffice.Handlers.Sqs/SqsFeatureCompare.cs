09b08da9d03f970030d6cdb3efa89f81a0ae7a1b
Rik De Peuter <rikdepeuter@gmail.com>
2022-11-08 15:18:36 +0100

    fix: Use SQS queue url instead of name when publishing

---
+++ src/RoadRegistry.BackOffice.Handlers.Sqs/SqsFeatureCompare.cs
@@ -0,0 +1,6 @@
+namespace RoadRegistry.BackOffice.Handlers.Sqs;
+
+internal static class SqsFeatureCompare
+{
+    public static readonly string MessageGroupId = Guid.Parse("1ea553f1-3af6-4594-85e8-ac9f463525d7").ToString();
+}
