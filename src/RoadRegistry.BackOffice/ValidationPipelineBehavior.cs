e7e9f103b3d8b6dde105910df23344d949a51ea4
Rik De Peuter <rikdepeuter@gmail.com>
2022-11-08 11:11:33 +0100

    fix: Use FluentValidation ValidateAsync

--- src/RoadRegistry.BackOffice/ValidationPipelineBehavior.cs
+++ src/RoadRegistry.BackOffice/ValidationPipelineBehavior.cs
@@ -17,12 +17,15 @@ public sealed class ValidationPipelineBehavior<TRequest, TResponse> : IPipelineB
         _validators = validators;
     }
 
-    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var context = new ValidationContext<TRequest>(request);
 
-        var failures = _validators
-            .Select(v => v.Validate(context))
+        var validationResults = await Task.WhenAll(_validators
+            .Select(v => v.ValidateAsync(context, cancellationToken))
+            .ToArray());
+
+        var failures = validationResults
             .SelectMany(result => result.Errors)
             .Where(f => f != null)
             .ToList();
@@ -30,6 +33,6 @@ public sealed class ValidationPipelineBehavior<TRequest, TResponse> : IPipelineB
         if (failures.Count != 0)
             throw new ValidationException(failures);
 
-        return next();
+        return await next();
     }
 }
