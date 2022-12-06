1fc7956752e2b2029718ed8575fc87e191217eaf
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2022-12-06 15:37:06 +0100

    feat: add road segment to snapshot producer

---
+++ src/RoadRegistry.Producer.Snapshot.ProjectionHost/RoadSegment/RoadSegmentProducerSnapshotContextMigrationFactory.cs
@@ -0,0 +1,33 @@
+namespace RoadRegistry.Producer.Snapshot.ProjectionHost.RoadSegment
+{
+    using Be.Vlaanderen.Basisregisters.ProjectionHandling.Runner;
+    using Hosts;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Infrastructure;
+
+    public class RoadSegmentProducerSnapshotContextMigrationFactory : RunnerDbContextMigrationFactory<RoadSegmentProducerSnapshotContext>
+    {
+        public RoadSegmentProducerSnapshotContextMigrationFactory() :
+            base(WellknownConnectionNames.ProducerSnapshotProjectionsAdmin, HistoryConfiguration)
+        {
+        }
+
+        private static MigrationHistoryConfiguration HistoryConfiguration =>
+            new()
+            {
+                Schema = WellknownSchemas.RoadSegmentProducerSnapshotSchema,
+                Table = MigrationTables.RoadSegmentProducerSnapshot
+            };
+
+        protected override void ConfigureSqlServerOptions(SqlServerDbContextOptionsBuilder sqlServerOptions)
+        {
+            sqlServerOptions.UseNetTopologySuite();
+            base.ConfigureSqlServerOptions(sqlServerOptions);
+        }
+
+        protected override RoadSegmentProducerSnapshotContext CreateContext(DbContextOptions<RoadSegmentProducerSnapshotContext> migrationContextOptions)
+        {
+            return new RoadSegmentProducerSnapshotContext(migrationContextOptions);
+        }
+    }
+}
---
+++ src/RoadRegistry.Producer.Snapshot.ProjectionHost/RoadSegment/RoadSegmentRecord.cs
@@ -0,0 +1,92 @@
+namespace RoadRegistry.Producer.Snapshot.ProjectionHost.RoadSegment
+{
+    using System;
+    using Be.Vlaanderen.Basisregisters.GrAr.Contracts.RoadRegistry;
+    using NetTopologySuite.Geometries;
+    using Schema;
+
+    public class RoadSegmentRecord
+    {
+        public string AccessRestrictionDutchName { get; set; }
+        public int? AccessRestrictionId { get; set; }
+        public string BeginApplication { get; set; }
+        public string BeginOperator { get; set; }
+        public string BeginOrganizationId { get; set; }
+        public string BeginOrganizationName { get; set; }
+        public int? BeginRoadNodeId { get; set; }
+        public DateTime? BeginTime { get; set; }
+        public string CategoryDutchName { get; set; }
+        public string CategoryId { get; set; }
+        public int? EndRoadNodeId { get; set; }
+        public Geometry Geometry { get; set; }
+        public int? GeometryVersion { get; set; }
+        public int Id { get; set; }
+        public int? LeftSideMunicipalityId { get; set; }
+        public string LeftSideMunicipalityNisCode { get; set; }
+        public string LeftSideStreetName { get; set; }
+        public int? LeftSideStreetNameId { get; set; }
+        public string MaintainerId { get; set; }
+        public string MaintainerName { get; set; }
+        public string MethodDutchName { get; set; }
+        public int? MethodId { get; set; }
+        public string MorphologyDutchName { get; set; }
+        public int? MorphologyId { get; set; }
+        public DateTime? RecordingDate { get; set; }
+        public int? RightSideMunicipalityId { get; set; }
+        public string RightSideMunicipalityNisCode { get; set; }
+        public string RightSideStreetName { get; set; }
+        public int? RightSideStreetNameId { get; set; }
+        public int? RoadSegmentVersion { get; set; }
+        public string StatusDutchName { get; set; }
+        public int? StatusId { get; set; }
+        public long StreetNameCachePosition { get; set; }
+        public int? TransactionId { get; set; }
+
+        public DateTimeOffset LastChangedTimestamp { get; set; }
+        public bool IsRemoved { get; set; }
+
+        // EF needs this
+        public RoadSegmentRecord() { }
+
+        public RoadSegmentSnapshot ToContract()
+        {
+            return new RoadSegmentSnapshot(
+                AccessRestrictionDutchName,
+                AccessRestrictionId,
+                BeginApplication,
+                BeginOperator,
+                BeginOrganizationId,
+                BeginOrganizationName,
+                BeginRoadNodeId,
+                BeginTime,
+                CategoryDutchName,
+                CategoryId,
+                EndRoadNodeId,
+                Geometry,
+                GeometryVersion,
+                Id,
+                LeftSideMunicipalityId,
+                LeftSideMunicipalityNisCode,
+                LeftSideStreetName,
+                LeftSideStreetNameId,
+                MaintainerId,
+                MaintainerName,
+                MethodDutchName,
+                MethodId,
+                MorphologyDutchName,
+                MorphologyId,
+                RecordingDate,
+                RightSideMunicipalityId,
+                RightSideMunicipalityNisCode,
+                RightSideStreetName,
+                RightSideStreetNameId,
+                RoadSegmentVersion,
+                StatusDutchName,
+                StatusId,
+                StreetNameCachePosition,
+                TransactionId,
+                LastChangedTimestamp,
+                IsRemoved);
+        }
+    }
+}
