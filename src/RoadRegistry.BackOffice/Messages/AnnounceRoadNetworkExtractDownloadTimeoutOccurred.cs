16f6f6b5643c97632f12f007d59482bbed294713
Jan Lesage <jan.lesage@vlaanderen.be>
2022-12-08 13:20:44 +0100

    fix: Removed TODO from AnnounceRoadNetworkExtractDownloadTimeoutOccurred message

--- src/RoadRegistry.BackOffice/Messages/AnnounceRoadNetworkExtractDownloadTimeoutOccurred.cs
+++ src/RoadRegistry.BackOffice/Messages/AnnounceRoadNetworkExtractDownloadTimeoutOccurred.cs
@@ -5,10 +5,4 @@ using Be.Vlaanderen.Basisregisters.EventHandling;
 public class AnnounceRoadNetworkExtractDownloadTimeoutOccurred : IMessage
 {
     public string RequestId { get; set; }
-
-    //TODO: - Extend all road network events with a RoadNetworkRevision
-    //      - Store this revision for each change in the editor projections
-    //      - Read this revision as part of assembling the archive to download
-    //      - Add this revision as a property here and copy over to the event
-    //      - Use the property to remember what revision of the road network we handed and to do conflict resolution
 }
