0d9cad6c1f79732d6236f885b5e5fa32c09ff76e
Rik De Peuter <rikdepeuter@gmail.com>
2022-12-09 16:27:44 +0100

    fix: add documentation for edit endpoint requests

--- src/RoadRegistry.BackOffice.Api/RoadSegments/RoadSegmentsController-PostLinkStreetName.cs
+++ src/RoadRegistry.BackOffice.Api/RoadSegments/RoadSegmentsController-PostLinkStreetName.cs
@@ -1,31 +1,47 @@
 namespace RoadRegistry.BackOffice.Api.RoadSegments;
 
+using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Abstractions.Exceptions;
 using Abstractions.RoadSegments;
 using Abstractions.Validation;
+using Amazon.S3.Model;
 using Be.Vlaanderen.Basisregisters.Api.Exceptions;
 using FeatureToggles;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Annotations;
 using Swashbuckle.AspNetCore.Filters;
 
 public partial class RoadSegmentsController
 {
+    /// <summary>
+    /// Koppel een straatnaam aan een wegsegment
+    /// </summary>
+    /// <param name="featureToggle"></param>
+    /// <param name="parameters"></param>
+    /// <param name="id">Identificator van het wegsegment.</param>
+    /// <param name="cancellationToken"></param>
+    /// <response code="200">Als het wegsegment gevonden is.</response>
+    /// <response code="404">Als het wegsegment niet gevonden kan worden.</response>
+    /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
+    /// <response code="500">Als er een interne fout is opgetreden.</response>
     [HttpPost("{id}/acties/straatnaamkoppelen")]
     [ProducesResponseType(typeof(LinkStreetNameResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-    [SwaggerResponseHeader(StatusCodes.Status200OK, "ETag", "string", "De ETag van de response.")]
+    [SwaggerRequestExample(typeof(PostLinkStreetNameParameters), typeof(PostLinkStreetNameParametersExamples))]
+    //[SwaggerResponseHeader(StatusCodes.Status202Accepted, "ETag", "string", "De ETag van de response.")]
     [SwaggerResponseHeader(StatusCodes.Status200OK, "x-correlation-id", "string", "Correlatie identificator van de response.")]
-    [SwaggerResponseExample(StatusCodes.Status200OK, typeof(LinkStreetNameResponseExamples))]
     [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
     [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(RoadSegmentNotFoundResponseExamples))]
     [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamples))]
     [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
+    [SwaggerOperation(Description = "Koppel een linker- en/of rechterstraatnaam met status `voorgesteld` of `inGebruik` aan een wegsegment waaraan momenteel geen linker- en/of rechterstraatnaam gekoppeld werd.")]
     public async Task<IActionResult> PostLinkStreetName(
         [FromServices] UseRoadSegmentLinkStreetNameFeatureToggle featureToggle,
         [FromBody] PostLinkStreetNameParameters parameters,
@@ -51,4 +67,31 @@ public partial class RoadSegmentsController
     }
 }
 
-public sealed record PostLinkStreetNameParameters(string? LinkerstraatnaamId, string? RechterstraatnaamId);
+[DataContract(Name = "StraatnaamKoppelen", Namespace = "")]
+public class PostLinkStreetNameParameters
+{
+    /// <summary>
+    /// De unieke en persistente identificator van de straatnaam aan de linkerzijde van het wegsegment.
+    /// </summary>
+    [DataMember(Name = "LinkerstraatnaamId", Order = 1)]
+    [JsonProperty(Required = Required.AllowNull)]
+    public string LinkerstraatnaamId { get; set; }
+
+    /// <summary>
+    /// De unieke en persistente identificator van de straatnaam aan de rechterzijde van het wegsegment.
+    /// </summary>
+    [DataMember(Name = "RechterstraatnaamId", Order = 2)]
+    [JsonProperty(Required = Required.AllowNull)]
+    public string RechterstraatnaamId { get; set; }
+}
+
+public class PostLinkStreetNameParametersExamples : IExamplesProvider<PostUnlinkStreetNameParameters>
+{
+    public PostUnlinkStreetNameParameters GetExamples()
+    {
+        return new PostUnlinkStreetNameParameters
+        {
+            LinkerstraatnaamId = "https://data.vlaanderen.be/id/straatnaam/23489"
+        };
+    }
+}
--- src/RoadRegistry.BackOffice.Api/RoadSegments/RoadSegmentsController-PostUnlinkStreetName.cs
+++ src/RoadRegistry.BackOffice.Api/RoadSegments/RoadSegmentsController-PostUnlinkStreetName.cs
@@ -1,5 +1,6 @@
 namespace RoadRegistry.BackOffice.Api.RoadSegments;
 
+using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Abstractions.Exceptions;
@@ -9,17 +10,34 @@ using Be.Vlaanderen.Basisregisters.Api.Exceptions;
 using FeatureToggles;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Swashbuckle.AspNetCore.Filters;
 
 public partial class RoadSegmentsController
 {
+    /// <summary>
+    /// Ontkoppel een straatnaam van een wegsegment
+    /// </summary>
+    /// <param name="featureToggle"></param>
+    /// <param name="parameters"></param>
+    /// <param name="id">Identificator van het wegsegment.</param>
+    /// <param name="cancellationToken"></param>
+    /// <response code="200">Als het wegsegment gevonden is.</response>
+    /// <response code="404">Als het wegsegment niet gevonden kan worden.</response>
+    /// <response code="429">Als het aantal requests per seconde de limiet overschreven heeft.</response>
+    /// <response code="500">Als er een interne fout is opgetreden.</response>
     [HttpPost("{id}/acties/straatnaamontkoppelen")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(UnlinkStreetNameResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status429TooManyRequests)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    [SwaggerRequestExample(typeof(PostUnlinkStreetNameParameters), typeof(PostUnlinkStreetNameParametersExamples))]
+    //[SwaggerResponseHeader(StatusCodes.Status202Accepted, "ETag", "string", "De ETag van de response.")]
+    [SwaggerResponseHeader(StatusCodes.Status200OK, "x-correlation-id", "string", "Correlatie identificator van de response.")]
     [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
+    [SwaggerResponseExample(StatusCodes.Status404NotFound, typeof(RoadSegmentNotFoundResponseExamples))]
+    [SwaggerResponseExample(StatusCodes.Status429TooManyRequests, typeof(TooManyRequestsResponseExamples))]
     [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
     public async Task<IActionResult> PostUnlinkStreetName(
         [FromServices] UseRoadSegmentUnlinkStreetNameFeatureToggle featureToggle,
@@ -46,4 +64,31 @@ public partial class RoadSegmentsController
     }
 }
 
-public sealed record PostUnlinkStreetNameParameters(string? LinkerstraatnaamId, string? RechterstraatnaamId);
+[DataContract(Name = "StraatnaamOntkoppelen", Namespace = "")]
+public class PostUnlinkStreetNameParameters
+{
+    /// <summary>
+    /// De unieke en persistente identificator van de straatnaam aan de linkerzijde van het wegsegment.
+    /// </summary>
+    [DataMember(Name = "LinkerstraatnaamId", Order = 1)]
+    [JsonProperty(Required = Required.AllowNull)]
+    public string LinkerstraatnaamId { get; set; }
+
+    /// <summary>
+    /// De unieke en persistente identificator van de straatnaam aan de rechterzijde van het wegsegment.
+    /// </summary>
+    [DataMember(Name = "RechterstraatnaamId", Order = 2)]
+    [JsonProperty(Required = Required.AllowNull)]
+    public string RechterstraatnaamId { get; set; }
+}
+
+public class PostUnlinkStreetNameParametersExamples : IExamplesProvider<PostUnlinkStreetNameParameters>
+{
+    public PostUnlinkStreetNameParameters GetExamples()
+    {
+        return new PostUnlinkStreetNameParameters
+        {
+            LinkerstraatnaamId = "https://data.vlaanderen.be/id/straatnaam/23489"
+        };
+    }
+}
