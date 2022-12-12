e3888a4da7a367c02d9263ac1fe66293edd07d89
Rik De Peuter <rikdepeuter@gmail.com>
2022-12-12 08:16:03 +0100

    fix: add edit-endpoint request examples + update Swagger decoration (#747)
    * fix: add street name link documentation
    
    * fix: add documentation for edit endpoint requests
    
    * fix: cleanup

--- src/RoadRegistry.BackOffice.Abstractions/RoadSegments/LinkStreetNameResponse.cs
+++ src/RoadRegistry.BackOffice.Abstractions/RoadSegments/LinkStreetNameResponse.cs
@@ -1,17 +1,3 @@
 namespace RoadRegistry.BackOffice.Abstractions.RoadSegments;
 
-using System.Runtime.Serialization;
-using Swashbuckle.AspNetCore.Filters;
-
-[DataContract(Name = "StraatnaamKoppelen", Namespace = "")]
-public sealed record LinkStreetNameResponse : EndpointResponse
-{
-}
-
-public class LinkStreetNameResponseExamples : IExamplesProvider<LinkStreetNameResponse>
-{
-    public LinkStreetNameResponse GetExamples()
-    {
-        return new LinkStreetNameResponse();
-    }
-}
+public sealed record LinkStreetNameResponse : EndpointResponse;
