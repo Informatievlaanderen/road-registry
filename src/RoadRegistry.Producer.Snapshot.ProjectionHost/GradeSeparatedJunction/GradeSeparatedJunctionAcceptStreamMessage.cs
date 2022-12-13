ad08816eeb2518f26fd11d2dab362238e6ede525
Rik De Peuter <rikdepeuter@gmail.com>
2022-12-13 11:24:30 +0100

    feat: add GradeSeparatedJunction snapshot projection

---
+++ src/RoadRegistry.Producer.Snapshot.ProjectionHost/GradeSeparatedJunction/GradeSeparatedJunctionAcceptStreamMessage.cs
@@ -0,0 +1,7 @@
+namespace RoadRegistry.Producer.Snapshot.ProjectionHost.GradeSeparatedJunction
+{
+    using Hosts;
+
+    public class GradeSeparatedJunctionAcceptStreamMessage : AcceptStreamMessage<GradeSeparatedJunctionProducerSnapshotContext>
+    { }
+}
