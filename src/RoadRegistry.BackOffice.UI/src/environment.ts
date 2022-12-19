1fad15bdae875eb901db3064c9cf514c3297ed08
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2022-12-19 15:26:04 +0100

    fix: UI change header color & title according to env

--- src/RoadRegistry.BackOffice.UI/src/environment.ts
+++ src/RoadRegistry.BackOffice.UI/src/environment.ts
@@ -1,6 +1,8 @@
+export const WR_ENV = window.WR_ENV || process.env.VUE_APP_WR_ENV;
 export const API_VERSION = window.API_VERSION || process.env.VUE_APP_API_VERSION;
 export const API_ENDPOINT = window.API_ENDPOINT || process.env.VUE_APP_API_ENDPOINT;
 export const API_OLDENDPOINT = window.API_OLDENDPOINT || process.env.VUE_APP_API_OLDENDPOINT;
 export const featureToggles = {
     useFeatureCompare: `${(window.featureToggles?.useFeatureCompare ?? process.env.VUE_APP_FEATURETOGGLES_USEFEATURECOMPARE)}` === 'true'
 };
+
