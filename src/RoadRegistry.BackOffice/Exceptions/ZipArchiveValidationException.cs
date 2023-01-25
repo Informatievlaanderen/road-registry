8cea4e6774673251db35ac512c419fa8bdff70bc
Rik De Peuter <rikdepeuter@gmail.com>
2023-01-25 14:20:14 +0100

    fix: WR-579 use translated messages for upload validation

---
+++ src/RoadRegistry.BackOffice/Exceptions/ZipArchiveValidationException.cs
@@ -0,0 +1,22 @@
+namespace RoadRegistry.BackOffice.Exceptions;
+
+using System;
+using System.Runtime.Serialization;
+using Uploads;
+
+[Serializable]
+public class ZipArchiveValidationException : RoadRegistryException
+{
+    public ZipArchiveProblems Problems { get; }
+
+    public ZipArchiveValidationException(ZipArchiveProblems problems)
+        : base("Zip archive is invalid.")
+    {
+        Problems = problems;
+    }
+
+    protected ZipArchiveValidationException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+    }
+}
