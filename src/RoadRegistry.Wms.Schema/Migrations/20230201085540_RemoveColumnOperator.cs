1c340f8b8672bcbaaa3e513a5830e84b7ac78f9b
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2023-02-01 09:59:07 +0100

    fix: Drop wms beginoperator column

---
+++ src/RoadRegistry.Wms.Schema/Migrations/20230201085540_RemoveColumnOperator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoadRegistry.Wms.Schema.Migrations
+{
+    public partial class RemoveColumnOperator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "beginoperator",
+                schema: "RoadRegistryWms",
+                table: "wegsegmentDenorm");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "beginoperator",
+                schema: "RoadRegistryWms",
+                table: "wegsegmentDenorm",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
