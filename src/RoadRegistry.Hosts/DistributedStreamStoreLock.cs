9bbf044a076c9b448c444a3d2c76e5bd3422358a
Rik De Peuter <rikdepeuter@gmail.com>
2023-02-07 15:43:12 +0100

    fix: update package Be.Vlaanderen.Basisregisters.Aws.DistributedMutex

--- src/RoadRegistry.Hosts/DistributedStreamStoreLock.cs
+++ src/RoadRegistry.Hosts/DistributedStreamStoreLock.cs
@@ -1,7 +1,5 @@
 namespace RoadRegistry.Hosts;
 
-using System;
-using System.Threading.Tasks;
 using BackOffice.Framework;
 using Be.Vlaanderen.Basisregisters.Aws.DistributedMutex;
 using Microsoft.Extensions.Logging;
@@ -10,13 +8,8 @@ using SqlStreamStore;
 public class DistributedStreamStoreLock : DistributedLock<IStreamStore>
 {
     public DistributedStreamStoreLock(DistributedStreamStoreLockOptions options, StreamName streamName, ILogger logger)
-        : base(options, logger) //TODO-rik after update use pass lockName to base
+        : base(options, $"{typeof(DistributedStreamStoreLock).Name}-{streamName}", logger)
     {
-    }
-
-    public Task RunAsync(Func<Task> runFunc) //TODO-rik mag weg na update package
-    {
-        return runFunc();
     }
 }
 
