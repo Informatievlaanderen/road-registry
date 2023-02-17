449cf43b83109ead35176d47efd4550bed6b7e28
Jan Lesage <jan.lesage@vlaanderen.be>
2023-02-17 10:29:40 +0100

    fix: Refactor program.cs for Wms ProjectionHost

--- src/RoadRegistry.Wms.ProjectionHost/Program.cs
+++ src/RoadRegistry.Wms.ProjectionHost/Program.cs
@@ -38,142 +38,69 @@ public class Program
 
     public static async Task Main(string[] args)
     {
-        Console.WriteLine("Starting RoadRegistry.Wms.ProjectionHost");
-
-        AppDomain.CurrentDomain.FirstChanceException += (sender, eventArgs) =>
-            Log.Debug(eventArgs.Exception, "FirstChanceException event raised in {AppDomain}.", AppDomain.CurrentDomain.FriendlyName);
-
-        AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
-            Log.Fatal((Exception)eventArgs.ExceptionObject, "Encountered a fatal exception, exiting program.");
-
-        var host = new HostBuilder()
-            .ConfigureHostConfiguration(builder =>
-            {
-                builder
-                    .AddEnvironmentVariables("DOTNET_")
-                    .AddEnvironmentVariables("ASPNETCORE_");
-            })
-            .ConfigureAppConfiguration((hostContext, builder) =>
-            {
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-                if (hostContext.HostingEnvironment.IsProduction())
-                    builder
-                        .SetBasePath(Directory.GetCurrentDirectory());
-
-                builder
-                    .AddJsonFile("appsettings.json", true, false)
-                    .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName.ToLowerInvariant()}.json", true, false)
-                    .AddJsonFile($"appsettings.{Environment.MachineName.ToLowerInvariant()}.json", true, false)
-                    .AddEnvironmentVariables()
-                    .AddCommandLine(args);
-            })
-            .ConfigureLogging((hostContext, builder) =>
-            {
-                SelfLog.Enable(Console.WriteLine);
-
-                var loggerConfiguration = new LoggerConfiguration()
-                    .ReadFrom.Configuration(hostContext.Configuration)
-                    .Enrich.FromLogContext()
-                    .Enrich.WithMachineName()
-                    .Enrich.WithThreadId()
-                    .Enrich.WithEnvironmentUserName();
-
-                Log.Logger = loggerConfiguration.CreateLogger();
-
-                builder.AddSerilog(Log.Logger);
-            })
-            .ConfigureServices((hostContext, builder) =>
-            {
-                builder
-                    .AddSingleton(provider => provider.GetRequiredService<IConfiguration>().GetSection(MetadataConfiguration.Section).Get<MetadataConfiguration>())
-                    .AddSingleton<IClock>(SystemClock.Instance)
-                    .AddSingleton<Scheduler>()
-                    .AddTransient<EventProcessor>()
-                    .AddSingleton<IStreetNameCache, StreetNameCache>()
-                    .AddScoped<IMetadataUpdater, MetadataUpdater>()
-                    .AddSingleton(new RecyclableMemoryStreamManager())
-                    .AddSingleton(new EnvelopeFactory(
-                        EventProcessor.EventMapping,
-                        new EventDeserializer((eventData, eventType) =>
-                            JsonConvert.DeserializeObject(eventData, eventType, EventProcessor.SerializerSettings)))
-                    )
-                    .AddSingleton(
-                        () =>
-                            new WmsContext(
-                                new DbContextOptionsBuilder<WmsContext>()
-                                    .UseSqlServer(
-                                        hostContext.Configuration.GetConnectionString(WellknownConnectionNames.WmsProjections),
-                                        options => options
-                                            .EnableRetryOnFailure()
-                                            .UseNetTopologySuite()
-                                    ).Options)
-                    )
-                    .AddSingleton(
-                        () =>
-                            new SyndicationContext(
-                                new DbContextOptionsBuilder<SyndicationContext>()
-                                    .UseSqlServer(
-                                        hostContext.Configuration.GetConnectionString(WellknownConnectionNames.SyndicationProjections),
-                                        options => options
-                                            .EnableRetryOnFailure()
-                                    ).Options)
-                    )
-                    .AddSingleton(sp => new ConnectedProjection<WmsContext>[]
-                    {
-                        new RoadSegmentRecordProjection(sp.GetRequiredService<IStreetNameCache>()),
-                        new RoadSegmentEuropeanRoadAttributeRecordProjection(),
-                        new RoadSegmentNationalRoadAttributeRecordProjection()
-                    })
-                    .AddSingleton(sp =>
-                        Resolve
-                            .WhenEqualToHandlerMessageType(
-                                sp.GetRequiredService<ConnectedProjection<WmsContext>[]>()
-                                    .SelectMany(projection => projection.Handlers)
-                                    .ToArray()
-                            )
-                    )
-                    .AddSingleton(sp => AcceptStreamMessage.WhenEqualToMessageType(sp.GetRequiredService<ConnectedProjection<WmsContext>[]>(), EventProcessor.EventMapping))
-                    .AddStreamStore()
-                    .AddSingleton<IRunnerDbContextMigratorFactory>(new WmsContextMigrationFactory());
-            })
+        var roadRegistryHost = new RoadRegistryHostBuilder<Program>(args)
+            .ConfigureServices((hostContext, services) => services
+                .AddSingleton(provider => provider.GetRequiredService<IConfiguration>().GetSection(MetadataConfiguration.Section).Get<MetadataConfiguration>())
+                .AddTransient<EventProcessor>()
+                .AddSingleton<IStreetNameCache, StreetNameCache>()
+                .AddScoped<IMetadataUpdater, MetadataUpdater>()
+                .AddSingleton(new EnvelopeFactory(
+                    EventProcessor.EventMapping,
+                    new EventDeserializer((eventData, eventType) =>
+                        JsonConvert.DeserializeObject(eventData, eventType, EventProcessor.SerializerSettings)))
+                )
+                .AddSingleton(
+                    () =>
+                        new WmsContext(
+                            new DbContextOptionsBuilder<WmsContext>()
+                                .UseSqlServer(
+                                    hostContext.Configuration.GetConnectionString(WellknownConnectionNames.WmsProjections),
+                                    options => options
+                                        .EnableRetryOnFailure()
+                                        .UseNetTopologySuite()
+                                ).Options)
+                )
+                .AddSingleton(
+                    () =>
+                        new SyndicationContext(
+                            new DbContextOptionsBuilder<SyndicationContext>()
+                                .UseSqlServer(
+                                    hostContext.Configuration.GetConnectionString(WellknownConnectionNames.SyndicationProjections),
+                                    options => options
+                                        .EnableRetryOnFailure()
+                                ).Options)
+                )
+                .AddSingleton(sp => new ConnectedProjection<WmsContext>[]
+                {
+                    new RoadSegmentRecordProjection(sp.GetRequiredService<IStreetNameCache>()),
+                    new RoadSegmentEuropeanRoadAttributeRecordProjection(),
+                    new RoadSegmentNationalRoadAttributeRecordProjection()
+                })
+                .AddSingleton(sp => Resolve.WhenEqualToHandlerMessageType(sp
+                    .GetRequiredService<ConnectedProjection<WmsContext>[]>()
+                    .SelectMany(projection => projection.Handlers)
+                    .ToArray())
+                )
+                .AddSingleton(sp => AcceptStreamMessage.WhenEqualToMessageType(sp.GetRequiredService<ConnectedProjection<WmsContext>[]>(), EventProcessor.EventMapping))
+                .AddSingleton<IRunnerDbContextMigratorFactory>(new WmsContextMigrationFactory()))
             .Build();
 
-        var migratorFactory = host.Services.GetRequiredService<IRunnerDbContextMigratorFactory>();
-        var configuration = host.Services.GetRequiredService<IConfiguration>();
-        var streamStore = host.Services.GetRequiredService<IStreamStore>();
-        var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
-        var logger = host.Services.GetRequiredService<ILogger<Program>>();
-        var eventProcessor = host.Services.GetRequiredService<EventProcessor>();
+        await roadRegistryHost
+            .LogSqlServerConnectionStrings(new[]
+            {
+                WellknownConnectionNames.Events,
+                WellknownConnectionNames.WmsProjections,
+                WellknownConnectionNames.WmsProjectionsAdmin
+            })
+            .RunAsync(async (sp, host, configuration) =>
+            {
+                var migratorFactory = host.Services.GetRequiredService<IRunnerDbContextMigratorFactory>();
+                var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
+                var eventProcessor = sp.GetRequiredService<EventProcessor>();
 
-        try
-        {
-            await WaitFor.SeqToBecomeAvailable(configuration).ConfigureAwait(false);
-
-            logger.LogSqlServerConnectionString(configuration, WellknownConnectionNames.Events);
-            logger.LogSqlServerConnectionString(configuration, WellknownConnectionNames.WmsProjections);
-            logger.LogSqlServerConnectionString(configuration, WellknownConnectionNames.WmsProjectionsAdmin);
-
-            await DistributedLock<Program>.RunAsync(async () =>
-                    {
-                        await WaitFor.SqlStreamStoreToBecomeAvailable(streamStore, logger).ConfigureAwait(false);
-                        await migratorFactory.CreateMigrator(configuration, loggerFactory)
-                            .MigrateAsync(CancellationToken.None).ConfigureAwait(false);
-
-                        Console.WriteLine("Started RoadRegistry.Wms.ProjectionHost");
-                        await eventProcessor.Resume(CancellationToken.None);
-                    },
-                    DistributedLockOptions.LoadFromConfiguration(configuration),
-                    logger)
-                .ConfigureAwait(false);
-        }
-        catch (Exception e)
-        {
-            logger.LogCritical(e, "Encountered a fatal exception, exiting program.");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
+                await migratorFactory.CreateMigrator(configuration, loggerFactory)
+                    .MigrateAsync(CancellationToken.None).ConfigureAwait(false);
+                await eventProcessor.Resume(CancellationToken.None);
+            });
     }
 }
