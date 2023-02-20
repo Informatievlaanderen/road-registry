5d628a3bbe10bf53856284554c2c4bba35fa5280
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2023-02-20 11:05:16 +0100

    fix: github action already has nvm installed by default

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 18.12.1
+nvm use 18.12.1
 dotnet tool restore
 dotnet paket restore
 chmod +x packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/*
