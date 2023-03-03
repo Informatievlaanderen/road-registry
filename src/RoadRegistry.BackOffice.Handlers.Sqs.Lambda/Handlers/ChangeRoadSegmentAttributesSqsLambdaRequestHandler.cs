86b761a139cb6ac59d5674803ec88ee4fd71b0a9
Jan Lesage <jan.lesage@vlaanderen.be>
2023-03-03 15:29:47 +0100

    fix: Add lambda handler type for ChangeRoadSegmentAttribute

--- src/RoadRegistry.BackOffice.Handlers.Sqs.Lambda/Handlers/ChangeRoadSegmentAttributesSqsLambdaRequestHandler.cs
+++ src/RoadRegistry.BackOffice.Handlers.Sqs.Lambda/Handlers/ChangeRoadSegmentAttributesSqsLambdaRequestHandler.cs
@@ -1,21 +1,14 @@
 namespace RoadRegistry.BackOffice.Handlers.Sqs.Lambda.Handlers;
 
-using Be.Vlaanderen.Basisregisters.Shaperon;
+using Abstractions.Exceptions;
 using Be.Vlaanderen.Basisregisters.Sqs.Lambda.Handlers;
 using Be.Vlaanderen.Basisregisters.Sqs.Lambda.Infrastructure;
 using Be.Vlaanderen.Basisregisters.Sqs.Responses;
-using Core;
-using Editor.Schema;
 using Hosts;
-using Hosts.Infrastructure.Extensions;
 using Infrastructure;
 using Microsoft.Extensions.Logging;
 using Requests;
 using TicketingService.Abstractions;
-using AddRoadSegment = BackOffice.Uploads.AddRoadSegment;
-using RoadSegmentLaneAttribute = BackOffice.Uploads.RoadSegmentLaneAttribute;
-using RoadSegmentSurfaceAttribute = BackOffice.Uploads.RoadSegmentSurfaceAttribute;
-using RoadSegmentWidthAttribute = BackOffice.Uploads.RoadSegmentWidthAttribute;
 
 public sealed class ChangeRoadSegmentAttributesSqsLambdaRequestHandler : SqsLambdaHandler<ChangeRoadSegmentAttributesSqsLambdaRequest>
 {
@@ -25,59 +18,33 @@ public sealed class ChangeRoadSegmentAttributesSqsLambdaRequestHandler : SqsLamb
         SqsLambdaHandlerOptions options,
         ICustomRetryPolicy retryPolicy,
         ITicketing ticketing,
-        IIdempotentCommandHandler idempotentCommandHandler,
-        IRoadNetworkEventWriter eventWriter,
         IRoadRegistryContext roadRegistryContext,
-        ILogger<CreateRoadSegmentOutlineSqsLambdaRequestHandler> logger)
+        IRoadNetworkEventWriter eventWriter,
+        ILogger<ChangeRoadSegmentAttributesSqsLambdaRequestHandler> logger)
         : base(
             options,
             retryPolicy,
             ticketing,
-            idempotentCommandHandler,
+            null,
             roadRegistryContext,
             logger)
     {
         _eventWriter = eventWriter;
     }
 
-    protected override Task<ETagResponse> InnerHandleAsync(ChangeRoadSegmentAttributesSqsLambdaRequest request, CancellationToken cancellationToken)
+    protected override async Task<ETagResponse> InnerHandleAsync(ChangeRoadSegmentAttributesSqsLambdaRequest request, CancellationToken cancellationToken)
     {
-        //    var changeRoadNetworkCommand = await _commandQueue.DispatchChangeRoadNetwork(IdempotentCommandHandler, request, "Wegsegment attributen wijzigen", async translatedChanges =>
-        //    {
-        //        var network = await RoadRegistryContext.RoadNetworks.Get(cancellationToken);
-        //        var roadSegmentId = network.ProvidesNextRoadSegmentId()();
+        var roadNetwork = await RoadRegistryContext.RoadNetworks.Get(cancellationToken);
+        var roadSegment = roadNetwork.FindRoadSegment(new RoadSegmentId(request.Request.WegsegmentId));
+        if (roadSegment == null)
+        {
+            throw new RoadSegmentNotFoundException();
+        }
 
-        //        var r = request.Request;
-        //        var recordNumber = RecordNumber.Initial;
 
-        //        var geometry = GeometryTranslator.Translate(r.Geometry);
 
-        //        var fromPosition = new RoadSegmentPosition(0);
-        //        var toPosition = new RoadSegmentPosition((decimal)geometry.Length);
-
-        //        translatedChanges = translatedChanges.AppendChange(
-        //            new AddRoadSegment(
-        //                    recordNumber,
-        //                    roadSegmentId,
-        //                    r.MaintenanceAuthority,
-        //                    RoadSegmentGeometryDrawMethod.Outlined,
-        //                    r.Morphology,
-        //                    r.Status,
-        //                    RoadSegmentCategory.Unknown,
-        //                    r.AccessRestriction)
-        //                .WithGeometry(geometry)
-        //                .WithSurface(new RoadSegmentSurfaceAttribute(network.ProvidesNextRoadSegmentSurfaceAttributeId()(roadSegmentId)(), r.SurfaceType, fromPosition, toPosition))
-        //                .WithWidth(new RoadSegmentWidthAttribute(network.ProvidesNextRoadSegmentWidthAttributeId()(roadSegmentId)(), r.Width, fromPosition, toPosition))
-        //                .WithLane(new RoadSegmentLaneAttribute(network.ProvidesNextRoadSegmentLaneAttributeId()(roadSegmentId)(), r.LaneCount, r.LaneDirection, fromPosition, toPosition))
-        //        );
-
-        //        return translatedChanges;
-        //    }, cancellationToken);
-
-        //    var roadSegmentId = new RoadSegmentId(changeRoadNetworkCommand.Changes.Single().AddRoadSegment.TemporaryId);
-        //    Logger.LogInformation("Created road segment {RoadSegmentId}", roadSegmentId);
-        //    var lastHash = await GetRoadSegmentHash(roadSegmentId, cancellationToken);
-        //    return new ETagResponse(string.Format(DetailUrlFormat, roadSegmentId), lastHash);
-        //}, cancellationToken);
+        var roadSegmentId = request.Request.WegsegmentId;
+        var lastHash = await GetRoadSegmentHash(new RoadSegmentId(roadSegmentId), cancellationToken);
+        return new ETagResponse(string.Format(DetailUrlFormat, roadSegmentId), lastHash);
     }
 }
