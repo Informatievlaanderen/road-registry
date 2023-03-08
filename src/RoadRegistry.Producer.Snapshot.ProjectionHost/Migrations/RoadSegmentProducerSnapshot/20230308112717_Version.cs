c9bfeb84d13a747c1e0ec7b23914a653091c5007
Luk Vermeulen <luk.vermeulen@gmail.com>
2023-03-08 12:33:59 +0100

    fix: add version property to RoadNode & RoadSegment Kafka projections

---
+++ src/RoadRegistry.Producer.Snapshot.ProjectionHost/Migrations/RoadSegmentProducerSnapshot/20230308112717_Version.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoadRegistry.Producer.Snapshot.ProjectionHost.Migrations.RoadSegmentProducerSnapshot
+{
+    public partial class Version : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                schema: "RoadRegistryRoadSegmentProducerSnapshot",
+                table: "RoadSegment",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                schema: "RoadRegistryRoadSegmentProducerSnapshot",
+                table: "RoadSegment");
+        }
+    }
+}
