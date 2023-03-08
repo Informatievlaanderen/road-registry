ff7a1e28cfafb6d549d3f10971996ee6f9e7741e
Rik De Peuter <rikdepeuter@gmail.com>
2023-03-08 16:15:52 +0100

    fix: WR-623 link/unlink streetname must have at least left or right streetnameid filled in

--- src/RoadRegistry.BackOffice.Handlers/RoadSegments/LinkStreetNameRequestValidator.cs
+++ src/RoadRegistry.BackOffice.Handlers/RoadSegments/LinkStreetNameRequestValidator.cs
@@ -4,9 +4,26 @@ using Abstractions.RoadSegments;
 using Abstractions.Validation;
 using Extensions;
 using FluentValidation;
+using FluentValidation.Results;
 
 public class LinkStreetNameRequestValidator : AbstractValidator<LinkStreetNameRequest>
 {
+    protected override bool PreValidate(ValidationContext<LinkStreetNameRequest> context, ValidationResult result)
+    {
+        if (context.InstanceToValidate.LinkerstraatnaamId.GetIdentifierFromPuri() <= 0
+            && context.InstanceToValidate.RechterstraatnaamId.GetIdentifierFromPuri() <= 0)
+        {
+            context.AddFailure(new ValidationFailure("request", ValidationErrors.Common.JsonInvalid.Message)
+            {
+                ErrorCode = ValidationErrors.Common.JsonInvalid.Code
+            });
+
+            return false;
+        }
+
+        return base.PreValidate(context, result);
+    }
+
     public LinkStreetNameRequestValidator()
     {
         RuleFor(x => x.WegsegmentId)
