b5f68059a62d52917f7db6687483bf2172e7ac55
Rik De Peuter <rikdepeuter@gmail.com>
2023-04-27 14:00:17 +0200

    fix: AdminHost read SQS messages correctly (#948)

--- src/RoadRegistry.AdminHost/Consumers/AdminMessageConsumer.cs
+++ src/RoadRegistry.AdminHost/Consumers/AdminMessageConsumer.cs
@@ -1,14 +1,16 @@
 namespace RoadRegistry.AdminHost.Consumers;
 
 using BackOffice;
-using MediatR;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
+using BackOffice.Abstractions;
 using BackOffice.Configuration;
 using Be.Vlaanderen.Basisregisters.MessageHandling.AwsSqs.Simple;
+using MediatR;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 public class AdminMessageConsumer
 {
@@ -32,15 +34,23 @@ public class AdminMessageConsumer
         _sqsOptions = sqsOptions;
         _logger = logger;
     }
-
+    
     public async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         try
         {
             var lastMessage = await _sqsConsumer.Consume(_sqsQueueUrlOptions.Admin, async message =>
             {
-                _logger.LogInformation("SQS message '{Type}' received", message.GetType().FullName);
-                var result = await _mediator.Send(message, stoppingToken);
+                var sqsMessageType = message.GetType();
+                _logger.LogInformation("SQS message '{Type}' received", sqsMessageType.FullName);
+
+                var backOfficeRequest = GetBackOfficeRequestFromSqsRequest(message);
+                if (backOfficeRequest is not null)
+                {
+                    _logger.LogInformation("Continuing with BackOffice request of type '{Type}'", backOfficeRequest.GetType().FullName);
+                }
+
+                var result = await _mediator.Send(backOfficeRequest ?? message, stoppingToken);
                 _logger.LogInformation("SQS message result: {Result}", JsonConvert.SerializeObject(result, _sqsOptions.JsonSerializerSettings));
             }, stoppingToken);
 
@@ -57,5 +67,22 @@ public class AdminMessageConsumer
         {
             _logger.LogCritical(ex, $"An unhandled exception has occurred: {ex.Message}");
         }
+    }
+
+    private static object GetBackOfficeRequestFromSqsRequest(object sqsMessage)
+    {
+        var backOfficeRequestType = sqsMessage.GetType()
+            .GetInterfaces()
+            .Where(iType => iType.IsGenericType && iType.GetGenericTypeDefinition() == typeof(IHasBackOfficeRequest<>))
+            .Select(iType => iType.GetGenericArguments()[0])
+            .FirstOrDefault();
+        
+        if (backOfficeRequestType is not null)
+        {
+            var requestProperty = sqsMessage.GetType().GetProperty(nameof(IHasBackOfficeRequest<object>.Request));
+            return requestProperty!.GetValue(sqsMessage);
+        }
+
+        return null;
     }
 }
