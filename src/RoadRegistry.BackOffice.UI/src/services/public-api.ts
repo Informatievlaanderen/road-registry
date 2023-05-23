7fe7e2d4549a7777b0cfd1c678c78315494430e2
Rik De Peuter <rikdepeuter@gmail.com>
2023-05-23 16:31:43 +0200

    fix: add featuretoggle for UI to use API directly instead of via nginx

--- src/RoadRegistry.BackOffice.UI/src/services/public-api.ts
+++ src/RoadRegistry.BackOffice.UI/src/services/public-api.ts
@@ -3,80 +3,82 @@ import RoadRegistry from "@/types/road-registry";
 import RoadRegistryExceptions from "@/types/road-registry-exceptions";
 import Municipalities from "@/types/municipalities";
 import axios from "axios";
+import { trimEnd } from 'lodash';
+import { featureToggles, API_ENDPOINT } from "@/environment";
 
-import { API_ENDPOINT } from "@/environment";
+const apiEndpoint = trimEnd(featureToggles.useDirectApiCalls ? API_ENDPOINT : '/public', '/');
 
 export const PublicApi = {
     ChangeFeed: {
         getHead: async (maxEntryCount: number): Promise<RoadRegistry.GetHeadApiResponse> => {
-            const path = `${API_ENDPOINT}/v1/wegen/activiteit/begin`;
+            const path = `${apiEndpoint}/v1/wegen/activiteit/begin`;
             const response = await apiClient.get<RoadRegistry.GetHeadApiResponse>(path, { maxEntryCount })
             return response.data as RoadRegistry.GetHeadApiResponse;
         },
         getContent: async (id: number): Promise<RoadRegistry.ChangeFeedContent> => {
-            const path = `${API_ENDPOINT}/v1/wegen/activiteit/gebeurtenis/${id}/inhoud`;
+            const path = `${apiEndpoint}/v1/wegen/activiteit/gebeurtenis/${id}/inhoud`;
             const response = await apiClient.get<RoadRegistry.ChangeFeedContent>(path)
             return response.data;
         },
         getNext: async (afterEntry?: number, maxEntryCount?: number) => {
-            const path = `${API_ENDPOINT}/v1/wegen/activiteit/volgende`;
+            const path = `${apiEndpoint}/v1/wegen/activiteit/volgende`;
             const response = await apiClient.get<RoadRegistry.GetHeadApiResponse>(path, { afterEntry, maxEntryCount })
             return response.data;
         },
         getPrevious: async (beforeEntry?: number, maxEntryCount?: number) => {
-            const path = `${API_ENDPOINT}/v1/wegen/activiteit/vorige`;
+            const path = `${apiEndpoint}/v1/wegen/activiteit/vorige`;
             const response = await apiClient.get<RoadRegistry.GetHeadApiResponse>(path, { beforeEntry, maxEntryCount })
             return response.data;
         }
     },
     Downloads: {
         getForEditor: async () => {
-            const path = `${API_ENDPOINT}/v1/wegen/download/voor-editor`;
+            const path = `${apiEndpoint}/v1/wegen/download/voor-editor`;
             await apiClient.download("application/zip", "wegenregister.zip", path, "GET")
         },
         getForProduct: async (date: string) => {
-            const path = `${API_ENDPOINT}/v1/wegen/download/voor-product/${date}`;
+            const path = `${apiEndpoint}/v1/wegen/download/voor-product/${date}`;
             await apiClient.download("application/zip", `wegenregister-${date}.zip`, path, "GET")
         },
     },
     Uploads: {
         upload: async (file: string | Blob, filename: string): Promise<boolean> => {
-            const path = `${API_ENDPOINT}/v1/wegen/upload`;
+            const path = `${apiEndpoint}/v1/wegen/upload`;
             const data = new FormData();
             data.append("archive", file, filename);
             const response = await apiClient.post(path,data)
             return response.status == 200;
         },
         download: async(identifier: string): Promise<void> => {
-            const path = `${API_ENDPOINT}/v1/wegen/upload/${identifier}`;
+            const path = `${apiEndpoint}/v1/wegen/upload/${identifier}`;
             await apiClient.download("application/zip", `${identifier}.zip`, path, "GET")
         }
     },
     Extracts: {
         download: async (downloadid: string) => {
-            const path = `${API_ENDPOINT}/v1/wegen/extract/download/${downloadid}`;
+            const path = `${apiEndpoint}/v1/wegen/extract/download/${downloadid}`;
             await apiClient.download("application/zip", `${downloadid}.zip`, path, "GET")
         },
         upload: async (downloadid: string, file: string | Blob, filename: string) => {
-            const path = `${API_ENDPOINT}/v1/wegen/extract/download/${downloadid}/uploads`;
+            const path = `${apiEndpoint}/v1/wegen/extract/download/${downloadid}/uploads`;
             const data = new FormData();
             data.append(downloadid, file, filename);
             const response = await apiClient.post<RoadRegistry.UploadExtractResponseBody>(path,data)
             return response.data;
         },
         getUploadStatus: async (uploadid: string): Promise<{status: string}> => {
-            const path = `${API_ENDPOINT}/v1/wegen/extract/upload/${uploadid}/status`;
+            const path = `${apiEndpoint}/v1/wegen/extract/upload/${uploadid}/status`;
             const response = await apiClient.get<{status: string}>(path);
             return response.data;
         },
         postDownloadRequest: async (downloadRequest: RoadRegistry.DownloadExtractRequest): Promise<RoadRegistry.DownloadExtractResponse> => {
-            const path = `${API_ENDPOINT}/v1/wegen/extract/downloadaanvragen`;
+            const path = `${apiEndpoint}/v1/wegen/extract/downloadaanvragen`;
             const response = await apiClient.post<RoadRegistry.DownloadExtractResponse>(path, downloadRequest)
             return response.data;
         },
         postDownloadRequestByContour: async (downloadRequest: RoadRegistry.DownloadExtractByContourRequest): Promise<RoadRegistry.DownloadExtractResponse> => {
             try {
-                const path = `${API_ENDPOINT}/v1/wegen/extract/downloadaanvragen/percontour`;
+                const path = `${apiEndpoint}/v1/wegen/extract/downloadaanvragen/percontour`;
                 const response = await apiClient.post<RoadRegistry.DownloadExtractResponse>(path, downloadRequest)
                 return response.data;
             } catch (exception) {
@@ -92,14 +94,14 @@ export const PublicApi = {
             }
         },
         postDownloadRequestByNisCode: async (downloadRequest: RoadRegistry.DownloadExtractByNisCodeRequest): Promise<RoadRegistry.DownloadExtractResponse> => {
-            const path = `${API_ENDPOINT}/v1/wegen/extract/downloadaanvragen/perniscode`;
+            const path = `${apiEndpoint}/v1/wegen/extract/downloadaanvragen/perniscode`;
             const response = await apiClient.post<RoadRegistry.DownloadExtractResponse>(path, downloadRequest)
             return response.data;
         }
     },
     Information: {
         getInformation: async (): Promise<RoadRegistry.RoadNetworkInformationResponse> => {
-            const path = `${API_ENDPOINT}/v1/wegen/informatie`;
+            const path = `${apiEndpoint}/v1/wegen/informatie`;
             const response = await apiClient.get<RoadRegistry.RoadNetworkInformationResponse>(path)
             return response.data;
         },
@@ -117,7 +119,7 @@ export const PublicApi = {
             };
 
             while(true) {
-                const response = (await apiClient.get<Municipalities.GetMunicipalitiesAPIResponse>(`${API_ENDPOINT}/v2/gemeenten`,query,headers)).data;
+                const response = (await apiClient.get<Municipalities.GetMunicipalitiesAPIResponse>(`${apiEndpoint}/v2/gemeenten`,query,headers)).data;
                 municipalities.push(...response.gemeenten)
                 if(!response.volgende) 
                     break;
