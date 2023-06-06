4b4a23593f17b16bd67e4269ca0a557466f351fe
Jan Lesage <jan.lesage@vlaanderen.be>
2023-06-06 15:51:36 +0200

    fix: Maximum lenght error inside unit test fixture

--- src/RoadRegistry.BackOffice.Abstractions/Extracts/DownloadExtractRequest.cs
+++ src/RoadRegistry.BackOffice.Abstractions/Extracts/DownloadExtractRequest.cs
@@ -1,5 +1,7 @@
 namespace RoadRegistry.BackOffice.Abstractions.Extracts;
 
+using System.ComponentModel.DataAnnotations;
+
 public sealed record DownloadExtractRequest(string RequestId, string Contour, bool IsInformative) : EndpointRequest<DownloadExtractResponse>
 {
 }
