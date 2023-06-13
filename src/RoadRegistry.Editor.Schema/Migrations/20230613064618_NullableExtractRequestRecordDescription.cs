a760d50ba85c78b03a07aebe4aa42eb688cd6d52
Jan Lesage <jan.lesage@vlaanderen.be>
2023-06-13 08:47:18 +0200

    fix: Make description inside the core extract request records nullable inside SQL

---
+++ src/RoadRegistry.Editor.Schema/Migrations/20230613064618_NullableExtractRequestRecordDescription.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoadRegistry.Editor.Schema.Migrations
+{
+    public partial class NullableExtractRequestRecordDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "RoadRegistryEditor",
+                table: "ExtractRequest",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "RoadRegistryEditor",
+                table: "ExtractRequest",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
