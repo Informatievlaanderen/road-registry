ded3549833d51e7e7ae61b45b52be9f75894061f
Rik De Peuter <rikdepeuter@gmail.com>
2023-06-14 11:54:54 +0200

    fix: WR-739 manual close request endpoint

--- src/RoadRegistry.BackOffice.Handlers/Extracts/CloseRoadNetworkExtractRequestHandler.cs
+++ src/RoadRegistry.BackOffice.Handlers/Extracts/CloseRoadNetworkExtractRequestHandler.cs
@@ -1,24 +1,37 @@
 namespace RoadRegistry.BackOffice.Handlers.Extracts;
 
 using Abstractions;
+using Abstractions.Exceptions;
 using Abstractions.Extracts;
+using Editor.Schema;
 using Framework;
 using Messages;
 using Microsoft.Extensions.Logging;
+using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
 
 public class CloseRoadNetworkExtractRequestHandler : EndpointRequestHandler<CloseRoadNetworkExtractRequest, CloseRoadNetworkExtractResponse>
 {
+    private readonly EditorContext _editorContext;
+
     public CloseRoadNetworkExtractRequestHandler(
         CommandHandlerDispatcher dispatcher,
+        EditorContext editorContext,
         ILogger<DownloadExtractByContourRequestHandler> logger) : base(dispatcher, logger)
     {
+        _editorContext = editorContext;
     }
 
     public override async Task<CloseRoadNetworkExtractResponse> HandleAsync(CloseRoadNetworkExtractRequest request, CancellationToken cancellationToken)
     {
+        var extractRequest = await _editorContext.ExtractRequests.FindAsync(request.DownloadId.ToGuid(), cancellationToken: cancellationToken);
+        if (extractRequest is null)
+        {
+            throw new UploadExtractNotFoundException("Could not close extract");
+        }
+
         var message = new CloseRoadNetworkExtract
         {
-            DownloadId = request.DownloadId,
+            ExternalRequestId = extractRequest.ExternalRequestId,
             Reason = request.Reason
         };
 
