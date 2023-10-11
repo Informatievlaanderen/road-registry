f3b3ede054a285466e1815856b07ebc344838ab3
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2023-10-11 08:22:45 +0200

    fix: use only net6 supported packages, fix mismatch versions between build-pipeline

--- build.fsx
+++ build.fsx
@@ -3,7 +3,37 @@ version 7.0.2
 framework: net6.0
 source https://api.nuget.org/v3/index.json
 
-nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 6.0.5 //"
+nuget System.Collections.Immutable 6.0.0
+nuget System.Configuration.ConfigurationManager 6.0.0
+nuget System.Reflection.Metadata 6.0.0
+nuget System.Reflection.MetadataLoadContext 6.0.0
+nuget System.Text.Encoding.CodePages 6.0.0
+nuget System.Text.Json 6.0.0
+nuget System.Threading.Tasks.Dataflow 6.0.0
+nuget System.Security.Cryptography.ProtectedData 6.0.0
+nuget System.Security.Cryptography.Pkcs 6.0.0
+nuget System.Security.Permissions 6.0.0
+nuget System.Formats.Asn1 6.0.0
+nuget System.Windows.Extensions 6.0.0
+nuget System.Drawing.Common 6.0.0
+nuget Microsoft.Win32.SystemEvents 6.0.0
+
+nuget Microsoft.NET.StringTools 17.3.2
+nuget Microsoft.NET.Test.Sdk 17.3.2
+nuget Microsoft.TestPlatform.TestHost 17.3.2
+nuget Microsoft.TestPlatform.ObjectModel 17.3.2
+nuget Microsoft.Build 17.3.2
+nuget Microsoft.Build.Framework 17.3.2
+nuget Microsoft.Build.Utilities.Core 17.3.2
+
+nuget NuGet.Common 6.4.0
+nuget NuGet.Frameworks 6.4.0
+nuget NuGet.Configuration 6.4.0
+nuget NuGet.Packaging 6.4.0
+
+nuget FSharp.Core 6.0
+
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 6.0.8 //"
 
 #load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
 
