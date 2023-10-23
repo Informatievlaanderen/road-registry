eec91cdcad84eb46430013fe5cc8604aea38dabe
Rik De Peuter <rikdepeuter@gmail.com>
2023-10-23 10:26:17 +0200

    fix: WR-868 remove default true for NameModified/OvoCodeModified (#1250)

--- src/RoadRegistry.BackOffice/Messages/ChangeOrganizationAccepted.cs
+++ src/RoadRegistry.BackOffice/Messages/ChangeOrganizationAccepted.cs
@@ -8,8 +8,8 @@ public class ChangeOrganizationAccepted : IMessage
 {
     public string Code { get; set; }
     public string Name { get; set; }
-    public bool NameModified { get; set; } = true; // default true for backwards compatibility
+    public bool NameModified { get; set; }
     public string OvoCode { get; set; }
-    public bool OvoCodeModified { get; set; } = true; // default true for backwards compatibility
+    public bool OvoCodeModified { get; set; }
     public string When { get; set; }
 }
