eb512a5aae72ae4d1380c101dc07d28d7c6010cb
Rik De Peuter <rikdepeuter@gmail.com>
2023-10-25 11:39:17 +0200

    fix: set AdminHost to AlwaysRunning for Development (#1257)

--- src/RoadRegistry.AdminHost/ExtractRequestCleanup.cs
+++ src/RoadRegistry.AdminHost/ExtractRequestCleanup.cs
@@ -10,40 +10,68 @@ namespace RoadRegistry.AdminHost
     using Editor.Schema;
     using Microsoft.EntityFrameworkCore;
     using System.Threading;
+    using Infrastructure.Options;
+    using Microsoft.Extensions.Logging;
 
     public class ExtractRequestCleanup
     {
         private readonly CommandHandlerDispatcher _dispatcher;
         private readonly EditorContext _editorContext;
+        private readonly AdminHostOptions _adminHostOptions;
+        private readonly ILogger<ExtractRequestCleanup> _logger;
 
         public ExtractRequestCleanup(
             CommandHandlerDispatcher commandHandlerDispatcher,
-            EditorContext editorContext)
+            EditorContext editorContext,
+            AdminHostOptions adminHostOptions,
+            ILogger<ExtractRequestCleanup> logger
+        )
         {
             _dispatcher = commandHandlerDispatcher;
             _editorContext = editorContext;
+            _adminHostOptions = adminHostOptions;
+            _logger = logger;
         }
 
         public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
-            var extractRequests = await _editorContext.ExtractRequests
-                .Where(extractRequest =>
-                    !extractRequest.IsInformative &&
-                    extractRequest.RequestedOn.Date <= DateTimeOffset.Now.Date.AddDays(-7) &&
-                    extractRequest.DownloadedOn == null)
-                .ToListAsync(cancellationToken);
-            
-            foreach (var extractRequest in extractRequests)
+            do
             {
-                var message = new CloseRoadNetworkExtract
+                try
                 {
-                    ExternalRequestId = new ExternalExtractRequestId(extractRequest.ExternalRequestId),
-                    Reason = RoadNetworkExtractCloseReason.NoDownloadReceived,
-                    DownloadId = new DownloadId(extractRequest.DownloadId)
-                };
-                var command = new Command(message);
-                await _dispatcher(command, cancellationToken);
-            }
+                    var extractRequests = await _editorContext.ExtractRequests
+                        .Where(extractRequest =>
+                            !extractRequest.IsInformative &&
+                            extractRequest.RequestedOn.Date <= DateTimeOffset.Now.Date.AddDays(-7) &&
+                            extractRequest.DownloadedOn == null)
+                        .ToListAsync(cancellationToken);
+
+                    foreach (var extractRequest in extractRequests)
+                    {
+                        _logger.LogInformation("Closing extract with DownloadId {DownloadId}, ExternalRequestId: {ExternalRequestId}", extractRequest.DownloadId, extractRequest.ExternalRequestId);
+
+                        var message = new CloseRoadNetworkExtract
+                        {
+                            ExternalRequestId = new ExternalExtractRequestId(extractRequest.ExternalRequestId),
+                            Reason = RoadNetworkExtractCloseReason.NoDownloadReceived,
+                            DownloadId = new DownloadId(extractRequest.DownloadId)
+                        };
+                        var command = new Command(message);
+                        await _dispatcher(command, cancellationToken);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogCritical(ex, $"An unhandled exception has occurred: {ex.Message}");
+                }
+                finally
+                {
+                    if (_adminHostOptions.AlwaysRunning && !cancellationToken.IsCancellationRequested)
+                    {
+                        await Task.Delay(new TimeSpan(1, 0, 0), cancellationToken);
+                    }
+                }
+            } while (_adminHostOptions.AlwaysRunning && !cancellationToken.IsCancellationRequested);
         }
     }
 }
