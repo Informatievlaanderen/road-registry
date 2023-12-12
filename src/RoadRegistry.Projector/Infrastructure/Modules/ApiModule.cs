ebc28f3d0dfa1e537e2db48b5f49502d46d6bb55
Rik De Peuter <rikdepeuter@gmail.com>
2023-12-12 13:21:56 +0100

    fix: WR-906 remove streetname atom feed from statuspage

--- src/RoadRegistry.Projector/Infrastructure/Modules/ApiModule.cs
+++ src/RoadRegistry.Projector/Infrastructure/Modules/ApiModule.cs
@@ -193,14 +193,6 @@ public class ApiModule : Module
             WellKnownConnectionName = WellknownConnectionNames.SyndicationProjections,
             IsSyndication = true
         });
-
-        RegisterProjection<SyndicationContext>(new ProjectionDetail
-        {
-            Id = "roadregistry-syndication-projectionhost-StraatNaam",
-            Name = "streetName",
-            WellKnownConnectionName = WellknownConnectionNames.SyndicationProjections,
-            IsSyndication = true
-        });
     }
 
     private void RegisterProjection<TContext>(ProjectionDetail projectionDetail) where TContext : DbContext
