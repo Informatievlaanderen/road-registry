7694e8055878b464e52663db6b6525d128d39909
jvandaal <jonas.vandaal@outlook.be>
2024-03-21 13:04:18 +0100

    chore: add assemblyinfo

--- src/RoadRegistry.Editor.Schema/Properties/AssemblyInfo.cs
+++ src/RoadRegistry.Editor.Schema/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyDescription("RoadRegistry Projector")]
+[assembly: AssemblyDescription("RoadRegistry Editor Schema")]
 
 [assembly: AssemblyProduct("Basisregisters Vlaanderen")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
-[assembly: ComVisible(false)]
 [assembly: ComVisible(false)]
--- src/RoadRegistry.Sync.OrganizationRegistry/Properties/AssemblyInfo.cs
+++ src/RoadRegistry.Sync.OrganizationRegistry/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyDescription("RoadRegistry Projector")]
+[assembly: AssemblyDescription("RoadRegistry Sync OrganizationRegistry")]
 
 [assembly: AssemblyProduct("Basisregisters Vlaanderen")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
-[assembly: ComVisible(false)]
 [assembly: ComVisible(false)]
