b2032d2f69382cac57af7ba23febc9b14da859f7
Rik De Peuter <rikdepeuter@gmail.com>
2024-05-30 11:11:26 +0200

    refactor: cleanup

--- src/RoadRegistry.BackOffice/WellKnownJsonConverters.cs
+++ src/RoadRegistry.BackOffice/WellKnownJsonConverters.cs
@@ -1,8 +1,6 @@
-using System.Collections.Generic;
-using System.Linq;
-
 namespace RoadRegistry.BackOffice
 {
+    using System.Collections.Generic;
     using Be.Vlaanderen.Basisregisters.Shaperon;
     using NetTopologySuite.Geometries;
     using NetTopologySuite.IO.Converters;
@@ -10,28 +8,23 @@ namespace RoadRegistry.BackOffice
 
     public static class WellKnownJsonConverters
     {
-        private static JsonConverter[] _converters;
+        public static readonly IReadOnlyCollection<JsonConverter> Converters;
 
         static WellKnownJsonConverters()
         {
-            var _factory = new GeometryFactory(new PrecisionModel(), SpatialReferenceSystemIdentifier.BelgeLambert1972.ToInt32());
+            var factory = new GeometryFactory(new PrecisionModel(), SpatialReferenceSystemIdentifier.BelgeLambert1972.ToInt32());
 
-            _converters = new JsonConverter[]
-            {
+            Converters =
+            [
                 new GeometryConverter(),
-                // new CoordinateConverter(), removed https://github.com/NetTopologySuite/NetTopologySuite.IO.GeoJSON/commit/52f33001e3f2536a3811abed6400dcfa2954dd7d
                 new FeatureCollectionConverter(),
                 new FeatureConverter(),
                 new AttributesTableConverter(),
-                new GeometryConverter(_factory, 2),
-                // new GeometryArrayConverter(_factory, 2), todo-rik
-                // new CoordinateConverter(_factory.PrecisionModel, 2),
+                new GeometryConverter(factory, 2),
                 new EnvelopeConverter(),
 
                 new GeoJSON.Net.Converters.GeoJsonConverter()
-            };
+            ];
         }
-
-        public static IReadOnlyCollection<JsonConverter> Converters => _converters.ToArray();
     }
 }
--- test/RoadRegistry.Tests/Framework/Projections/Formatters.cs
+++ test/RoadRegistry.Tests/Framework/Projections/Formatters.cs
@@ -6,23 +6,20 @@ using Newtonsoft.Json;
 
 public static class Formatters
 {
-    private static readonly GeometryFactory _factory = new(new PrecisionModel(), 31370);
+    private static readonly GeometryFactory Factory = new(new PrecisionModel(), 31370);
     private static readonly JsonConverter[] Converters;
 
     static Formatters()
     {
-        Converters = new JsonConverter[]
-        {
+        Converters =
+        [
             new GeometryConverter(),
-            // new CoordinateConverter(), removed https://github.com/NetTopologySuite/NetTopologySuite.IO.GeoJSON/commit/52f33001e3f2536a3811abed6400dcfa2954dd7d
             new FeatureCollectionConverter(),
             new FeatureConverter(),
             new AttributesTableConverter(),
-            new GeometryConverter(_factory, 2),
-            // new GeometryArrayConverter(_factory, 2), todo-rik
-            // new CoordinateConverter(_factory.PrecisionModel, 2),
+            new GeometryConverter(Factory, 2),
             new EnvelopeConverter()
-        };
+        ];
     }
 
     public static string NamedJsonMessage<T>(T message)
