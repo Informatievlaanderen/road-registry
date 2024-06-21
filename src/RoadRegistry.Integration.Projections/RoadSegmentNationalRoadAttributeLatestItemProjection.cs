2e41918f514f36e0e393cf3fc35aa3083d24a9a4
jvandaal <jonas.vandaal@outlook.be>
2024-06-21 16:11:39 +0200

    feat: national road attribute latest item projection

---
+++ src/RoadRegistry.Integration.Projections/RoadSegmentNationalRoadAttributeLatestItemProjection.cs
@@ -0,0 +1,86 @@
+namespace RoadRegistry.Integration.Projections;
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using BackOffice.Extensions;
+using BackOffice.Messages;
+using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
+using Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore;
+using Schema;
+using Schema.RoadSegments;
+
+public class RoadSegmentNationalRoadAttributeLatestItemProjection : ConnectedProjection<IntegrationContext>
+{
+    public RoadSegmentNationalRoadAttributeLatestItemProjection()
+    {
+        When<Envelope<ImportedRoadSegment>>((context, envelope, token) =>
+        {
+            if (envelope.Message.PartOfNationalRoads.Length == 0)
+                return Task.CompletedTask;
+
+            var nationalRoadAttributes = envelope.Message
+                .PartOfNationalRoads
+                .Select(nationalRoad => new RoadSegmentNationalRoadAttributeLatestItem
+                {
+                    Id = nationalRoad.AttributeId,
+                    RoadSegmentId = envelope.Message.Id,
+                    Number = nationalRoad.Number,
+                    BeginOrganizationId = envelope.Message.Origin.OrganizationId,
+                    BeginOrganizationName = envelope.Message.Origin.Organization,
+                    CreatedOnTimestamp = new DateTimeOffset(envelope.Message.RecordingDate),
+                    VersionTimestamp = new DateTimeOffset(envelope.Message.Origin.Since)
+                });
+
+            return context.RoadSegmentNationalRoadAttributes.AddRangeAsync(nationalRoadAttributes, token);
+        });
+
+        When<Envelope<RoadNetworkChangesAccepted>>(async (context, envelope, token) =>
+        {
+            foreach (var change in envelope.Message.Changes.Flatten())
+                switch (change)
+                {
+                    case RoadSegmentAddedToNationalRoad nationalRoad:
+                        await context.RoadSegmentNationalRoadAttributes.AddAsync(new RoadSegmentNationalRoadAttributeLatestItem
+                        {
+                            Id = nationalRoad.AttributeId,
+                            RoadSegmentId = nationalRoad.SegmentId,
+                            Number = nationalRoad.Number,
+                            BeginOrganizationId = envelope.Message.OrganizationId,
+                            BeginOrganizationName = envelope.Message.Organization,
+                            CreatedOnTimestamp = LocalDateTimeTranslator.TranslateFromWhen(envelope.Message.When),
+                            VersionTimestamp = LocalDateTimeTranslator.TranslateFromWhen(envelope.Message.When)
+                        });
+                        break;
+                    case RoadSegmentRemovedFromNationalRoad nationalRoad:
+                        var latestItem = await context.RoadSegmentNationalRoadAttributes
+                            .FindAsync(nationalRoad.AttributeId, cancellationToken: token)
+                            .ConfigureAwait(false);
+
+                        if (latestItem is not null)
+                        {
+                            latestItem.IsRemoved = true;
+                            latestItem.BeginOrganizationId = envelope.Message.OrganizationId;
+                            latestItem.BeginOrganizationName = envelope.Message.Organization;
+                            latestItem.VersionTimestamp = LocalDateTimeTranslator.TranslateFromWhen(envelope.Message.When);
+                        }
+                        break;
+
+                    case RoadSegmentRemoved roadSegmentRemoved:
+                        var latestItemsToRemove =
+                            await context.RoadSegmentNationalRoadAttributes.IncludeLocalToListAsync(
+                                q => q.Where(x => x.RoadSegmentId == roadSegmentRemoved.Id),
+                                token);
+
+                        foreach (var latestItemToRemove in latestItemsToRemove)
+                        {
+                            latestItemToRemove.IsRemoved = true;
+                            latestItemToRemove.BeginOrganizationId = envelope.Message.OrganizationId;
+                            latestItemToRemove.BeginOrganizationName = envelope.Message.Organization;
+                            latestItemToRemove.VersionTimestamp = LocalDateTimeTranslator.TranslateFromWhen(envelope.Message.When);
+                        }
+                        break;
+                }
+        });
+    }
+}
---
+++ test/RoadRegistry.Integration.ProjectionHost.Tests/Projections/RoadSegmentNationalRoadAttributeRecordProjectionTests.cs
@@ -0,0 +1,218 @@
+namespace RoadRegistry.Integration.ProjectionHost.Tests.Projections;
+
+using AutoFixture;
+using BackOffice;
+using BackOffice.Messages;
+using Integration.Projections;
+using RoadRegistry.Tests.BackOffice;
+using Schema.RoadSegments;
+
+public class RoadSegmentNationalRoadAttributeLatestItemProjectionTests
+{
+    private readonly Fixture _fixture;
+
+    public RoadSegmentNationalRoadAttributeLatestItemProjectionTests()
+    {
+        _fixture = new Fixture();
+        _fixture.CustomizeArchiveId();
+        _fixture.CustomizeAttributeId();
+        _fixture.CustomizeRoadSegmentId();
+        _fixture.CustomizeRoadNodeId();
+        _fixture.CustomizeOrganizationId();
+        _fixture.CustomizeOrganizationName();
+        _fixture.CustomizePolylineM();
+        _fixture.CustomizeEuropeanRoadNumber();
+        _fixture.CustomizeNationalRoadNumber();
+        _fixture.CustomizeNumberedRoadNumber();
+        _fixture.CustomizeRoadSegmentNumberedRoadDirection();
+        _fixture.CustomizeRoadSegmentNumberedRoadOrdinal();
+        _fixture.CustomizeRoadSegmentLaneCount();
+        _fixture.CustomizeRoadSegmentLaneDirection();
+        _fixture.CustomizeRoadSegmentWidth();
+        _fixture.CustomizeRoadSegmentSurfaceType();
+        _fixture.CustomizeRoadSegmentGeometryDrawMethod();
+        _fixture.CustomizeRoadSegmentMorphology();
+        _fixture.CustomizeRoadSegmentStatus();
+        _fixture.CustomizeRoadSegmentCategory();
+        _fixture.CustomizeRoadSegmentAccessRestriction();
+        _fixture.CustomizeRoadSegmentGeometryVersion();
+
+        _fixture.CustomizeImportedRoadSegment();
+        _fixture.CustomizeImportedRoadSegmentEuropeanRoadAttributes();
+        _fixture.CustomizeImportedRoadSegmentNationalRoadAttributes();
+        _fixture.CustomizeImportedRoadSegmentNumberedRoadAttributes();
+        _fixture.CustomizeImportedRoadSegmentLaneAttributes();
+        _fixture.CustomizeImportedRoadSegmentWidthAttributes();
+        _fixture.CustomizeImportedRoadSegmentSurfaceAttributes();
+        _fixture.CustomizeImportedRoadSegmentSideAttributes();
+        _fixture.CustomizeOriginProperties();
+
+        _fixture.CustomizeRoadNetworkChangesAccepted();
+
+        _fixture.CustomizeRoadSegmentAddedToNationalRoad();
+        _fixture.CustomizeRoadSegmentRemovedFromNationalRoad();
+        _fixture.CustomizeRoadSegmentRemoved();
+    }
+
+    [Fact]
+    public Task When_adding_road_segments()
+    {
+        var message = _fixture
+            .Create<RoadNetworkChangesAccepted>()
+            .WithAcceptedChanges(_fixture.CreateMany<RoadSegmentAddedToNationalRoad>());
+
+        var expectedRecords = Array.ConvertAll(message.Changes, change =>
+        {
+            var nationalRoad = change.RoadSegmentAddedToNationalRoad;
+
+            return (object)new RoadSegmentNationalRoadAttributeLatestItem
+            {
+                Id = nationalRoad.AttributeId,
+                RoadSegmentId = nationalRoad.SegmentId,
+                Number = nationalRoad.Number,
+                BeginOrganizationId = message.OrganizationId,
+                BeginOrganizationName = message.Organization,
+                CreatedOnTimestamp = LocalDateTimeTranslator.TranslateFromWhen(message.When),
+                VersionTimestamp = LocalDateTimeTranslator.TranslateFromWhen(message.When)
+            };
+        });
+
+        return new RoadSegmentNationalRoadAttributeLatestItemProjection()
+            .Scenario()
+            .Given(message)
+            .Expect(expectedRecords);
+    }
+
+    [Fact]
+    public Task When_importing_a_road_node_without_national_road_links()
+    {
+        var importedRoadSegment = _fixture.Create<ImportedRoadSegment>();
+        importedRoadSegment.PartOfNationalRoads = [];
+
+        return new RoadSegmentNationalRoadAttributeLatestItemProjection()
+            .Scenario()
+            .Given(importedRoadSegment)
+            .Expect([]);
+    }
+
+    [Fact]
+    public Task When_importing_road_nodes()
+    {
+        var random = new Random();
+        var data = _fixture
+            .CreateMany<ImportedRoadSegment>(random.Next(1, 10))
+            .Select(segment =>
+            {
+                segment.PartOfNationalRoads = _fixture
+                    .CreateMany<ImportedRoadSegmentNationalRoadAttribute>(random.Next(1, 10))
+                    .ToArray();
+
+                var expected = segment
+                    .PartOfNationalRoads
+                    .Select(nationalRoad => new RoadSegmentNationalRoadAttributeLatestItem
+                    {
+                        Id = nationalRoad.AttributeId,
+                        RoadSegmentId = segment.Id,
+                        Number = nationalRoad.Number,
+                        BeginOrganizationId = segment.Origin.OrganizationId,
+                        BeginOrganizationName = segment.Origin.Organization,
+                        CreatedOnTimestamp = new DateTimeOffset(segment.RecordingDate),
+                        VersionTimestamp = new DateTimeOffset(segment.Origin.Since)
+                    });
+
+                return new
+                {
+                    importedRoadSegment = segment,
+                    expected
+                };
+            }).ToList();
+
+        return new RoadSegmentNationalRoadAttributeLatestItemProjection()
+            .Scenario()
+            .Given(data.Select(d => d.importedRoadSegment))
+            .Expect(data
+                .SelectMany(d => d.expected)
+                .Cast<object>()
+                .ToArray()
+            );
+    }
+
+    [Fact]
+    public Task When_removing_road_segments()
+    {
+        var roadSegmentAddedToNationalRoad = _fixture.Create<RoadSegmentAddedToNationalRoad>();
+        var anotherRoadSegmentAddedToNationalRoad = _fixture.Create<RoadSegmentAddedToNationalRoad>();
+
+        var acceptedRoadSegmentsAdded = _fixture
+            .Create<RoadNetworkChangesAccepted>()
+            .WithAcceptedChanges(
+                roadSegmentAddedToNationalRoad,
+                anotherRoadSegmentAddedToNationalRoad);
+
+        var acceptedRoadSegmentRemoved = _fixture
+            .Create<RoadNetworkChangesAccepted>()
+            .WithAcceptedChanges(new RoadSegmentRemoved
+            {
+                Id = anotherRoadSegmentAddedToNationalRoad.SegmentId
+            });
+
+        return new RoadSegmentNationalRoadAttributeLatestItemProjection()
+            .Scenario()
+            .Given(acceptedRoadSegmentsAdded, acceptedRoadSegmentRemoved)
+            .Expect([
+                new RoadSegmentNationalRoadAttributeLatestItem
+                {
+                    Id = roadSegmentAddedToNationalRoad.AttributeId,
+                    RoadSegmentId = roadSegmentAddedToNationalRoad.SegmentId,
+                    Number = roadSegmentAddedToNationalRoad.Number,
+                    IsRemoved = false,
+                    BeginOrganizationId = acceptedRoadSegmentsAdded.OrganizationId,
+                    BeginOrganizationName = acceptedRoadSegmentsAdded.Organization,
+                    CreatedOnTimestamp = LocalDateTimeTranslator.TranslateFromWhen(acceptedRoadSegmentsAdded.When),
+                    VersionTimestamp = LocalDateTimeTranslator.TranslateFromWhen(acceptedRoadSegmentsAdded.When)
+                },
+                new RoadSegmentNationalRoadAttributeLatestItem
+                {
+                    Id = anotherRoadSegmentAddedToNationalRoad.AttributeId,
+                    RoadSegmentId = anotherRoadSegmentAddedToNationalRoad.SegmentId,
+                    Number = anotherRoadSegmentAddedToNationalRoad.Number,
+                    IsRemoved = true,
+                    BeginOrganizationId = acceptedRoadSegmentRemoved.OrganizationId,
+                    BeginOrganizationName = acceptedRoadSegmentRemoved.Organization,
+                    CreatedOnTimestamp = LocalDateTimeTranslator.TranslateFromWhen(acceptedRoadSegmentsAdded.When),
+                    VersionTimestamp = LocalDateTimeTranslator.TranslateFromWhen(acceptedRoadSegmentRemoved.When)
+                }]);
+    }
+
+    [Fact]
+    public Task When_removing_road_segments_from_national_roads()
+    {
+        _fixture.Freeze<AttributeId>();
+
+        var roadSegmentAddedToNationalRoad = _fixture.Create<RoadSegmentAddedToNationalRoad>();
+        var acceptedRoadSegmentAdded = _fixture
+            .Create<RoadNetworkChangesAccepted>()
+            .WithAcceptedChanges(roadSegmentAddedToNationalRoad);
+
+        var acceptedRoadSegmentRemoved = _fixture
+            .Create<RoadNetworkChangesAccepted>()
+            .WithAcceptedChanges(_fixture.Create<RoadSegmentRemovedFromNationalRoad>());
+
+        return new RoadSegmentNationalRoadAttributeLatestItemProjection()
+            .Scenario()
+            .Given(acceptedRoadSegmentAdded, acceptedRoadSegmentRemoved)
+            .Expect([
+                new RoadSegmentNationalRoadAttributeLatestItem
+                {
+                    Id = roadSegmentAddedToNationalRoad.AttributeId,
+                    RoadSegmentId = roadSegmentAddedToNationalRoad.SegmentId,
+                    Number = roadSegmentAddedToNationalRoad.Number,
+                    IsRemoved = true,
+                    BeginOrganizationId = acceptedRoadSegmentRemoved.OrganizationId,
+                    BeginOrganizationName = acceptedRoadSegmentRemoved.Organization,
+                    CreatedOnTimestamp = LocalDateTimeTranslator.TranslateFromWhen(acceptedRoadSegmentAdded.When),
+                    VersionTimestamp = LocalDateTimeTranslator.TranslateFromWhen(acceptedRoadSegmentRemoved.When)
+                }
+            ]);
+    }
+}
