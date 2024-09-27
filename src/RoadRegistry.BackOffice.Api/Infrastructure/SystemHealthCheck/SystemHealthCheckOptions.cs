9f60519967ba5b1777fd8cec3f8864e651d15f50
Rik De Peuter <rikdepeuter@gmail.com>
2024-09-27 15:43:44 +0200

    fix: increase system healthcheck timeout (#1550)

--- src/RoadRegistry.BackOffice.Api/Infrastructure/SystemHealthCheck/SystemHealthCheckOptions.cs
+++ src/RoadRegistry.BackOffice.Api/Infrastructure/SystemHealthCheck/SystemHealthCheckOptions.cs
@@ -6,4 +6,5 @@ using System.Collections.Generic;
 public sealed class SystemHealthCheckOptions
 {
     public required ICollection<Type> HealthCheckTypes { get; init; }
+    public TimeSpan CheckTimeout { get; set; } = TimeSpan.FromMinutes(2);
 }
