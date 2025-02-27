3d3fe66d235b7283558956517f9b20663a82a56f
Rik De Peuter <rikdepeuter@gmail.com>
2025-02-27 11:16:24 +0100

    refactor(wms): merge TransactionZoneEventProcessor into WmsContextEventProcessor

---
+++ src/RoadRegistry.Wms.Schema/Migrations/20250227101159_RemoveTransactionZoneEventProcessor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoadRegistry.Wms.Schema.Migrations
+{
+    using BackOffice;
+
+    /// <inheritdoc />
+    public partial class RemoveTransactionZoneEventProcessor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM [{WellKnownSchemas.WmsMetaSchema}].[ProjectionStates] WHERE [Name] = 'roadregistry-wms-transactionzone-projectionhost'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
