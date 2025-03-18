7ba90e1cdd41828ff1895bbfdfa005388a706e54
Rik De Peuter <rikdepeuter@gmail.com>
2025-03-18 13:33:54 +0100

    cleanup

--- src/RoadRegistry.BackOffice.Abstractions/Exceptions/RoadSegmentsNotFoundException.cs
+++ src/RoadRegistry.BackOffice.Abstractions/Exceptions/RoadSegmentsNotFoundException.cs
@@ -8,7 +8,7 @@ public class RoadSegmentsNotFoundException : RoadRegistryException
 {
     public ICollection<RoadSegmentId> RoadSegmentIds { get; }
 
-    public RoadSegmentsNotFoundException(ICollection<RoadSegmentId> roadSegmentIds) : base("Road segments could not be found.")
+    public RoadSegmentsNotFoundException(ICollection<RoadSegmentId> roadSegmentIds) : base($"Road segments could not be found: {string.Join(",", roadSegmentIds)}")
     {
         RoadSegmentIds = roadSegmentIds;
     }
