59143414895dc64d478fb97637a801b24f13dabd
Rik De Peuter <rikdepeuter@gmail.com>
2025-03-28 11:30:35 +0100

    add ef migrations

---
+++ src/RoadRegistry.Product.Schema/Migrations/20250328102453_AddVersionToRoadNetworkInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoadRegistry.Product.Schema.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVersionToRoadNetworkInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastChangedTimestamp",
+                schema: "RoadRegistryProduct",
+                table: "RoadNetworkInfo",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: DateTimeOffset.UtcNow);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastChangedTimestamp",
+                schema: "RoadRegistryProduct",
+                table: "RoadNetworkInfo");
+        }
+    }
+}
