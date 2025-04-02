13dfe8f545466a2bff1197b80a0d967afe44b718
Rik De Peuter <rikdepeuter@gmail.com>
2025-04-02 10:51:32 +0200

    re-add obsolete shapefile package

--- src/RoadRegistry.BackOffice/ShapeFile/ZipArchiveShapeFileReader.cs
+++ src/RoadRegistry.BackOffice/ShapeFile/ZipArchiveShapeFileReader.cs
@@ -8,6 +8,7 @@ using System.IO.Compression;
 using System.Linq;
 using NetTopologySuite.IO.Esri.Shapefiles.Readers;
 using NetTopologySuite.IO.Esri.Shp;
+using NetTopologySuite.IO.Streams;
 
 public class ZipArchiveShapeFileReader
 {
@@ -27,6 +28,30 @@ public class ZipArchiveShapeFileReader
         }).ToArray();
 
         foreach (var geometry in geometries)
+        {
+            yield return (geometry, recordNumber);
+            recordNumber = recordNumber.Next();
+        }
+    }
+}
+
+public class ZipArchiveShapeFileReaderV1
+{
+    public IEnumerable<(Geometry, RecordNumber)> Read(ZipArchiveEntry entry)
+    {
+        var recordNumber = RecordNumber.Initial;
+
+        using var fileStream = entry.Open();
+        using var memoryStream = new MemoryStream();
+
+        fileStream.CopyTo(memoryStream);
+        memoryStream.Position = 0;
+
+        var streamProvider = new ExternallyManagedStreamProvider(StreamTypes.Shape, memoryStream);
+        var streamProviderRegistry = new ShapefileStreamProviderRegistry(streamProvider, null);
+        var shpReader = new NetTopologySuite.IO.ShapeFile.Extended.ShapeReader(streamProviderRegistry);
+
+        foreach (var geometry in shpReader.ReadAllShapes(WellKnownGeometryFactories.Default))
         {
             yield return (geometry, recordNumber);
             recordNumber = recordNumber.Next();
