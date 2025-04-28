b43a4992fd8a47722241dda7e87e4c191e916a7e
Rik De Peuter <rikdepeuter@gmail.com>
2025-04-28 14:44:39 +0200

    revert ef snapshot

--- src/RoadRegistry.Integration.Schema/Migrations/IntegrationContextModelSnapshot.cs
+++ src/RoadRegistry.Integration.Schema/Migrations/IntegrationContextModelSnapshot.cs
@@ -18,7 +18,7 @@ namespace RoadRegistry.Integration.Schema.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.4")
+                .HasAnnotation("ProductVersion", "8.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.HasPostgresExtension(modelBuilder, "postgis");
