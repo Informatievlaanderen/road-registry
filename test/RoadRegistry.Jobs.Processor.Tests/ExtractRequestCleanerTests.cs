de51cfac4020c9fdd3a8c2c0990711b146686731
Rik De Peuter <rikdepeuter@gmail.com>
2025-06-25 11:46:57 +0200

    refactor: move extract cleanup to JobsProcessor

---
+++ test/RoadRegistry.Jobs.Processor.Tests/ExtractRequestCleanerTests.cs
@@ -0,0 +1,98 @@
+﻿namespace RoadRegistry.Jobs.Processor.Tests;
+
+using BackOffice;
+using BackOffice.Messages;
+using Editor.Schema;
+using Editor.Schema.Extracts;
+using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Point = NetTopologySuite.Geometries.Point;
+
+public class ExtractRequestCleanerTests
+{
+    [Fact]
+    public async Task GivenDownloadedExtracts_ThenCloseExtractsOlderThan6Months()
+    {
+        // Arrange
+        var commandHandlerDispatcher = new FakeCommandHandlerDispatcher();
+        await using var editorContext = new FakeEditorContextFactory().CreateDbContext();
+
+        var oldExtractDownloadId = Guid.NewGuid();
+        editorContext.ExtractRequests.Add(new ExtractRequestRecord
+        {
+            DownloadId = oldExtractDownloadId,
+            ExternalRequestId = "external-request-id-1",
+            RequestedOn = DateTimeOffset.Now.AddMonths(-7),
+            DownloadedOn = DateTimeOffset.Now,
+            IsInformative = false,
+            Contour = new Point(0, 0)
+        });
+        editorContext.ExtractRequests.Add(new ExtractRequestRecord
+        {
+            DownloadId = Guid.NewGuid(),
+            ExternalRequestId = "external-request-id-2",
+            RequestedOn = DateTimeOffset.Now.AddMonths(-1),
+            DownloadedOn = DateTimeOffset.Now,
+            IsInformative = false,
+            Contour = new Point(0, 0)
+        });
+        await editorContext.SaveChangesAsync();
+
+        var cleaner = new ExtractRequestCleaner(
+            commandHandlerDispatcher.Dispatcher,
+            editorContext,
+            new NullLoggerFactory()
+        );
+
+        // Act
+        await cleaner.CloseOldExtracts(CancellationToken.None);
+
+        // Assert
+        commandHandlerDispatcher.Invocations.Should().NotBeEmpty();
+        var command = commandHandlerDispatcher.Invocations.Single().Body.Should().BeOfType<CloseRoadNetworkExtract>().Subject;
+        command.DownloadId.Should().Be(new DownloadId(oldExtractDownloadId));
+    }
+
+    [Fact]
+    public async Task GivenNotDownloadedExtracts_ThenCloseExtractsOlderThanAWeek()
+    {
+        // Arrange
+        var commandHandlerDispatcher = new FakeCommandHandlerDispatcher();
+        await using var editorContext = new FakeEditorContextFactory().CreateDbContext();
+
+        var oldExtractDownloadId = Guid.NewGuid();
+        editorContext.ExtractRequests.Add(new ExtractRequestRecord
+        {
+            DownloadId = oldExtractDownloadId,
+            ExternalRequestId = "external-request-id-1",
+            RequestedOn = DateTimeOffset.Now.AddDays(-8),
+            DownloadedOn = null,
+            IsInformative = false,
+            Contour = new Point(0, 0)
+        });
+        editorContext.ExtractRequests.Add(new ExtractRequestRecord
+        {
+            DownloadId = Guid.NewGuid(),
+            ExternalRequestId = "external-request-id-2",
+            RequestedOn = DateTimeOffset.Now.AddMonths(-1),
+            DownloadedOn = DateTimeOffset.Now,
+            IsInformative = false,
+            Contour = new Point(0, 0)
+        });
+        await editorContext.SaveChangesAsync();
+
+        var cleaner = new ExtractRequestCleaner(
+            commandHandlerDispatcher.Dispatcher,
+            editorContext,
+            new NullLoggerFactory()
+        );
+
+        // Act
+        await cleaner.CloseOldExtracts(CancellationToken.None);
+
+        // Assert
+        commandHandlerDispatcher.Invocations.Should().NotBeEmpty();
+        var command = commandHandlerDispatcher.Invocations.Single().Body.Should().BeOfType<CloseRoadNetworkExtract>().Subject;
+        command.DownloadId.Should().Be(new DownloadId(oldExtractDownloadId));
+    }
+}
