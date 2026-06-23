38c5d6361ae11e1be92a54adbd47ffc192ca7f9d
Rik De Peuter <rikdepeuter@gmail.com>
2026-06-23 10:40:11 +0200

    fix: optionally disable marten migration projection

--- src/RoadRegistry.MartenMigration.Projections/SetupExtensions.cs
+++ src/RoadRegistry.MartenMigration.Projections/SetupExtensions.cs
@@ -1,15 +1,32 @@
 ﻿namespace RoadRegistry.MartenMigration.Projections;
 
+using System;
 using BackOffice;
 using Hosts.Infrastructure.Extensions;
 using Infrastructure.MartenDb.Setup;
 using Marten;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 public static class SetupExtensions
 {
-    public static void AddMartenDbMigrationEventProcessor(this IServiceCollection services)
+    public static void AddMartenDbMigrationEventProcessor(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString(WellKnownConnectionNames.Marten);
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            services.AddSingleton<Action>(sp =>
+            {
+                return () =>
+                {
+                    var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+                    loggerFactory.CreateLogger("MartenDbMigrationEventProcessor").LogError("Marten migration event processor is disabled.");
+                };
+            });
+            return;
+        }
+
         services
             .AddSingleton<ConfigureDbContextOptionsBuilder<MartenMigrationContext>>(MartenMigrationContext.ConfigureOptions)
             .AddDbContext<MartenMigrationContext>((sp, options) => sp.GetRequiredService<ConfigureDbContextOptionsBuilder<MartenMigrationContext>>()(sp, options))
